56867783af24a9fb91aba12cf14b9e172220fddb
rog-won <looky1254@naver.com>
2024-03-13 20:56:18 +0900

    #3 메인페이지 추가작업 중간 커밋.
    메인페이지 추가작업 중간 커밋.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/tailwindcss/output.css';  
+import "./styles/tailwindcss/input.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/main/Header.js
@@ -0,0 +1,45 @@
+function Header() {
+    return (
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+            <nav className="relative z-50 flex justify-between">
+                <div className="flex items-center md:gap-x-12">
+                    <a href="/">
+                        svg
+                    </a>
+                    <div className="hidden md:flex md:gap-x-6">
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#features">서비스소개</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#testimonials">상품</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">매장찾기</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">서비스</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">이벤트</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">창업안내</a>
+                    </div>
+                </div>
+                <div className="flex items-center gap-x-5 md:gap-x-8">
+                    <div className="hidden md:block">
+                        <a className="inline-block rounded-lg px-2 py-1 text-2xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/login">Sign in</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-2xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/login">sign in1</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-2xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/login">Sign in2</a>
+                    </div>
+                    {/* <a className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600" color="blue" variant="solid" href="/register">
+                        <span>Get started 
+                            <span class="hidden lg:inline">today</span>
+                        </span></a> */}
+                    <div className="-mr-1 md:hidden">
+                        <div data-headlessui-state="">
+                            <button className="relative z-10 flex h-8 w-8 items-center justify-center ui-not-focus-visible:outline-none" aria-label="Toggle Navigation" type="button" aria-expanded="false" data-headlessui-state="" id="headlessui-popover-button-:Rbpnla:">
+                                <svg aria-hidden="true" className="h-3.5 w-3.5 overflow-visible stroke-slate-700" fill="none" strokeWidth="2" strokeLinecap="round">
+                                    <path d="M0 1H14M0 7H14M0 13H14" className="origin-center transition"></path>
+                                    <path d="M2 2L12 12M12 2L2 12" className="origin-center transition scale-90 opacity-0"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <div style={{ position: 'fixed', top: 1, left: 1, width: 1, height: 0, padding: 0, margin: -1, overflow: 'hidden', clip: 'rect(0, 0, 0, 0)', whiteSpace: 'nowrap', borderWidth: 0, display: 'none' }}></div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
