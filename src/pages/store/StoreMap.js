a49333c66e5f979e2168124546fc4e20ec0ee02a
jisu <parangwavetraveller@gmail.com>
2024-03-15 11:26:47 +0900

    #4 feat: 지도 위 마커 수정
    지도 위 매장 마커 icon 수정

--- src/pages/store/StoreMap.js
+++ src/pages/store/StoreMap.js
@@ -10,43 +10,53 @@ import {
 // import axios from 'axios';
 
 import MapData from "../../assets/data/store/emart24_busan.json";
+import MarkerImg from '../../assets/imgs/store/marker.svg';
+
 
 export default function StoreMap(prop) {
   function MyMap() {
     // instead of window.naver.maps
     const navermaps = useNavermaps();
-    let name = "ssg";
 
     return (
       <NaverMap
         // defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
         // defaultCenter={new navermaps.LatLng(prop.location.latitude, prop.location.longitude)}
         defaultCenter={new navermaps.LatLng(35.16591583, 129.1324683)}
-        defaultZoom={8}
+        defaultZoom={17}
       >
         <Marker
           position={new navermaps.LatLng(35.16591583, 129.1324683)}
-          animation={0}
+          animation={1}
           icon={{
-            content: `<button class="markerBox" style="background-color: yellow;" >
-                <div class="totalOrder">1</div>
-                ${name}</button>`,
+            content: `<button class="markerBox" style="font-size: 30px">
+                        🙋‍♂️
+                      </button>`,
+            // onAnimationChanged: 
           }}
+          // icon={{
+          //   url: damgom,
+          //   size: new navermaps.Size(40, 40),
+          //   origin: new navermaps.Point(0, 0),
+          //   anchor: new navermaps.Point(11, 35),
+          //   // onAnimationChanged: 
+          // }}
         />
-        {MapData.map(function(mark, i) {
+        {
+          MapData.map(function (mark, i) {
             return (
-            <Marker
-              position={new navermaps.LatLng(MapData[i].위도, MapData[i].경도)}
-              animation={0}
-              icon={{
-                content: `<button class="markerBox" style="background-color: red;" >
-                  <div class="totalOrder">1</div>
-                  ${MapData[i].지점명}</button>`,
-              }}
-            />
-            )
-          }
-        )
+              <Marker
+                position={new navermaps.LatLng(mark.latitude, mark.longtitude)}
+                animation={0}
+                icon={{
+                  url: MarkerImg,  // 아이콘 경로
+                  size: new navermaps.Size(30, 40), //아이콘 크기
+	        	      origin: new navermaps.Point(0, 0),
+	        	      // anchor: new navermaps.Point(11, 35),
+                }}
+              />
+            );
+          })
         }
       </NaverMap>
     );
