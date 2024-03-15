163b88e7716cb16479cd56c3456e7815682fb447
hakiseong <ks510222@naver.com>
2024-03-15 23:02:45 +0900

    이상한거 건드려져서 수정했어요
    이상한거 건드려져서 수정했어요

--- src/pages/main/Header.js
+++ src/pages/main/Header.js
@@ -9,10 +9,10 @@ function Header() {
                     <div className="hidden md:flex md:gap-x-6">
                         <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#features">서비스소개</a>
                         <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/productlist">상품</a>
-                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">매장찾기</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/store">매장찾기</a>
                         <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">서비스</a>
                         <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">이벤트</a>
-                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">창업안내</a>
+                        <a className="inline-block rounded-lg px-2 py-1 text-3xl text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/ceo">발주하자</a>
                     </div>
                     
                 </div>
