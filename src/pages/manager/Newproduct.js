2a5179aa9284fd2cb7028bf2a848b62ea51bb066
hakiseong <ks510222@naver.com>
2024-03-17 16:41:28 +0900

    #16 fix 신제품등록 조금 수정
    url 입력하면 사진 잘뜨는데 상품리스트 볼때 엑박으로 뜨고, uload 폴더에 저장되있는사진도 엑박으로 저장됩니다....

--- src/pages/manager/Newproduct.js
+++ src/pages/manager/Newproduct.js
@@ -8,30 +8,37 @@ function Newproduct() {
     
     const [name,setName] = useState("");
     const [price,setPrice] = useState("");
-    const [url,setUrl] = useState("");
 
-    function newproductinsert(){
-        axios.get("http://localhost:8080/api/v1/product/newproductinsert", 
-                {params: {"name":name, 
-                          "price":price,
-                          "url":url}})
-            .then(function(resp){
-                console.log(resp.data);
-                if(resp.data === "YES") {
-                    alert("등록되었습니다.");
-                    navigate("/manager");
-                }
-                else {
-                    alert("등록 실패");
-                    navigate("/manager");
-                }
-            })
-    }
+
+    const onSubmit = (e) => {
+        e.preventDefault(); // 이동하지 않도록 하는 함수
+    
+        // 짐을 싼다
+        let formData = new FormData();
+        formData.append("name", name);
+        formData.append("price", price);
+    
+        // file -> byte
+        formData.append("uploadfile",document.frm.uploadfile.files[0]);
+    
+        // 보내자
+        // multipart인 경우에는 두번째 매개변수 보내는 파라미터가 된다
+        axios.post("http://localhost:8080/api/v1/product/newproductinsert", formData)
+       .then((res) => {
+            console.log(res.data);
+            navigate("/productlist");
+          })
+       .catch((err) => {
+            console.log(err);
+          });
+      }
  
     return(
         <>
         <div>신제품 등록 페이지 입니다..</div>
+        <form name="frm" onSubmit={onSubmit} encType="multipart/form-data">
         <table>
+            <tbody>
             <tr>
                 <td>상품명</td>
                 <td><input type='text' placeholder='상품명을 입력하세요.'
@@ -44,12 +51,13 @@ function Newproduct() {
             </tr>
             <tr>
                 <td>상품 이미지</td>
-                <td><input type='text' placeholder='상품이미지 URL을 입력하세요.'
-                value={url} onChange={(e)=>setUrl(e.target.value)}/></td>
+                <td><input type='file' name='uploadfile' /></td>
             </tr>
+            </tbody>
         </table>
         <br/>
-        <button onClick={()=>newproductinsert()}>상품 등록</button>
+        <input type="submit" value="상품등록" />
+        </form>
 
         </>
     )
