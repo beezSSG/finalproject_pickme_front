7f3e846f2510827f1f68b37de5faea4ac27897e2
jisu <parangwavetraveller@gmail.com>
2024-03-19 03:45:21 +0900

    #10 style: AdProductSet.js style 수정, Swiper.js 적용, 리팩토링
    Material UI로 구현한 slider를 Swiper.js로 교체 및 style 수정
    sematic tag를 활용하여 리팩토링
    
    Co-Authored-By: Mingi Kang <37468959+KangGuide@users.noreply.github.com>
    Co-Authored-By: hakiseong <149459133+hakiseong@users.noreply.github.com>

--- src/pages/main/AdProductSet.js
+++ src/pages/main/AdProductSet.js
@@ -1,30 +1,155 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "../../styles/main/PromotionStyle.css";
+
+// import required modules
+import { Autoplay, Keyboard, Pagination, Navigation } from "swiper/modules";
+
 const AdProductSet = () => {
   return (
-    <div className="">
-      <div className="bg-white shadow-2xl rounded-2xl m-auto mb-11">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <>
+      <div className="bg-white rounded-2xl m-auto mb-11 drop-shadow-2xl">
+        <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="flex justify-between">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900">
               이달의 행사 상품
-            </h2>
+            </h1>
             <h2 className="text-2xl font-bold tracking-tight text-gray-900">
               <button>더보기</button>
             </h2>
           </div>
+          {/* 상품 분류 태그 */}
           <span>
-            <button style={{ backgroundColor: "red" }}>1+1</button>
+            <button>1+1</button>
           </span>
           &nbsp;&nbsp;
           <span>
-            <button style={{ backgroundColor: "blue" }}>2+1</button>
+            <button>2+1</button>
           </span>
           &nbsp;&nbsp;
           <span>
-            <button style={{ backgroundColor: "green" }}>신상품</button>
+            <button>신상품</button>
           </span>
-          &nbsp;
-          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            <div className="group relative">
+          {/* 상품 목록 */}
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={20}
+            loop={true}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            keyboard={{
+              enabled: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            // navigation={true}
+            modules={[Autoplay, Keyboard, Pagination]}
+            className="mySwiper AdProductSet__Slider"
+          >
+            <SwiperSlide className="productItem flex flex-col" width={100}>
+              <img
+                src="https://www.emart24.co.kr/image/MTA1NDc1"
+                alt="Front of men's Basic Tee in black."
+              />
+              <div className="productItem__description">
+                <h4>조지아)리치아로마스위트270ml</h4>
+                <h3 className="font-bold text-xl">2400원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA2MDUx"
+                alt="Front of men's Basic Tee in white."
+              />
+              <div className="productItem__description">
+                <h4>슈가로로)스파클링복숭아사이다350ml</h4>
+                <h3 className="font-bold text-xl">1800원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA1OTI1"
+                alt="Front of men's Basic Tee in black."
+              />
+              <div className="productItem__description">
+                <h4>동원)보성홍차아이스티복숭아500ml</h4>
+                <h3 className="font-bold text-xl">2200원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA3MjAz"
+                alt="Front of men's Basic Tee in dark gray."
+              />
+              <div className="productItem__description">
+                <h4>후버)파인애플&사과&포도주스200ml(S)</h4>
+                <h3 className="font-bold text-xl">1200원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA3MjA1"
+                alt="Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube."
+              />
+              <div className="productItem__description">
+                <h4>후버)복숭아&포도주스200ml(S)</h4>
+                <h3 className="font-bold text-xl">1200원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA2NjMx"
+                alt="Front of men's Basic Tee in white."
+              />
+              <div className="productItem__description">
+                <h4>츄파춥스)사워바이츠60g</h4>
+                <h3 className="font-bold text-xl">1400원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA2MTM5"
+                alt="Front of men's Basic Tee in dark gray."
+              />
+              <div className="productItem__description">
+                <h4>오뚜기)맛있는오곡밥210g</h4>
+                <h3 className="font-bold text-xl">2700원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA1ODUw"
+                alt="Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube."
+              />
+              <div className="productItem__description">
+                <h4>조지아)리치아로마스위트270ml</h4>
+                <h3 className="font-bold text-xl">2400원</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="productItem flex flex-col">
+              <img
+                src="https://www.emart24.co.kr/image/MTA1OTI0"
+                alt="Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube."
+              />
+              <div className="productItem__description">
+                <h4>동원)보성홍차아이스티레몬500ml</h4>
+                <h3 className="font-bold text-xl">2200원</h3>
+              </div>
+            </SwiperSlide>
+            <br />
+            <br />
+          </Swiper>
+          {/* <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            <div className="group relative ">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
                   src="https://www.emart24.co.kr/image/MTA1NDc1"
@@ -48,7 +173,7 @@ const AdProductSet = () => {
                 <p className="text-sm font-medium text-gray-900">40,000원</p>
               </div>
             </div>
-            <div className="group relative">
+            <div className="group relative productItem">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
                   src="https://www.emart24.co.kr/image/MTA2MDUx"
@@ -72,7 +197,7 @@ const AdProductSet = () => {
                 <p className="text-sm font-medium text-gray-900">30,000원</p>
               </div>
             </div>
-            <div className="group relative">
+            <div className="group relative productItem">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
                   src="https://www.emart24.co.kr/image/MTA1NDAx"
@@ -96,7 +221,7 @@ const AdProductSet = () => {
                 <p className="text-sm font-medium text-gray-900">25,000원</p>
               </div>
             </div>
-            <div className="group relative">
+            <div className="group relative productItem">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
                   src="https://www.emart24.co.kr/image/MTA1ODUw"
@@ -120,10 +245,10 @@ const AdProductSet = () => {
                 <p className="text-sm font-medium text-gray-900">10,000원</p>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default AdProductSet;
