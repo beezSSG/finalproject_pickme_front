2be03cd84ca7da6da6a4efedd63090ae04fcdc99
GONZOZO <149459193+GONZOZO@users.noreply.github.com>
2024-03-18 17:03:23 +0900

    #27
    인기순, 등록순 검색 추가
    상품 목록에 평점 추가

--- src/pages/product/Productlist.js
+++ src/pages/product/Productlist.js
@@ -6,6 +6,8 @@ import Pagination from 'react-js-pagination'; // npm i react-js-pagination
 function Productlist() {
     const [productlist, setProductlist] = useState([]);
     
+    // 정렬
+    const [choice, setChoice] = useState("");
     // 검색  
     const [search, setSearch] = useState("");
     // 페이징
@@ -15,9 +17,9 @@ function Productlist() {
 
     /* Axios를 사용하여 서버에서 데이터를 가져오기 위한 비동기 함수, fetchData */
     
-    function getProductlist(s, pn){
+    function getProductlist(c, s, pn){
         axios.get("http://localhost:8080/api/v1/product/productlist", 
-                    {params:{ search:s, pageNumber:pn}})
+                    {params:{ choice:c, search:s, pageNumber:pn}})
              .then(function(resp){  // success:function
                 console.log(resp.data.productlist);
                 setProductlist(resp.data.productlist);
@@ -29,17 +31,27 @@ function Productlist() {
     }
 
     useEffect(function(){
-        getProductlist('', 0);
+        getProductlist('', '', 0);
     }, []);
 
 
+    function choiceBtn(choice){
+
+      if(search === ''){
+        getProductlist(choice, '', 0);
+        return;
+      }
+
+      getProductlist(choice, search, 0);
+    }
+
     function searchBtn(){        
-        getProductlist(search, 0);
+        getProductlist('select', search, 0);
     }
     
     function handlePageChange(page){
         setPage(page);
-        getProductlist(search, page-1);
+        getProductlist('', search, page-1);
     }
 
 
@@ -49,12 +61,19 @@ function Productlist() {
         <tbody>
             <tr>
                 <td style={{ paddingLeft:"5px"}} className='align-middle'>
-                    <input className='form-control' placeholder='검색어' 
+                    <input placeholder='검색어' 
                         value={search} onChange={(e)=>{setSearch(e.target.value)}} />
                 </td>
                 <td style={{ paddingLeft:"5px" }}>  
-                    <button className='btn btn-primary' onClick={searchBtn}>검색</button>
-                </td>
+                    <button onClick={()=>searchBtn()}>검색</button>
+                  </td>
+                  <td style={{ paddingLeft:"5px" }}>
+                      <button className='btn btn-secondary' onClick={() => choiceBtn('date')}>등록순</button>
+                  </td>
+                  <td style={{ paddingLeft:"5px" }}>
+                      <button className='btn btn-secondary' onClick={() => choiceBtn('rate')}>평점순</button>
+                  </td>
+                
             </tr>                
         </tbody>    
       </table>
@@ -62,9 +81,13 @@ function Productlist() {
       <h3>전체 상품 목록</h3>
 
       {<table border="1">
+      <colgroup>
+                <col width="70"/><col width="500"/><col width="100"/><col width="150"/>
+      </colgroup>
+
         <thead>
           <tr>
-          <th>제품 사진</th><th>제품명</th><th>가격</th>
+          <th>제품 사진</th><th>제품명</th><th>가격</th><th>평점</th>
           </tr>
         </thead>
 
@@ -76,12 +99,19 @@ function Productlist() {
                 for (let i = 0; i < productlist.length; i++) {
                   const product = productlist[i];
                   rows.push(
-                    <tr key={product.id}>
-                      <Link to={`/productdetail/${product.id}`}>
-                        <img src={product.url} style={{ maxWidth: '100px', maxHeight: '100px', margin: '10px' }} />
-                      </Link>
+                    <tr key={product.id} style={{textAlign:'center'}}>
+                      <td>
+                        <Link to={`/productdetail/${product.id}`}>
+                          <img src={product.url} style={{ maxWidth: '100px', maxHeight: '100px', margin: '10px' }} />
+                        </Link>
+                      </td>
                       <td>{product.name}</td>
                       <td>{product.price}</td>
+                      <td>
+                          {Array.from({ length: product.productRating }, (_, index) => (
+                              <span key={index}>★</span>
+                          ))}
+                      </td>
                       </tr>
                   );
                 }
