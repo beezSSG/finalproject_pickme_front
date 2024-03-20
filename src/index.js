d82e21e922120bcec130c9b05cc8052595806d45
rog-won <looky1254@naver.com>
2024-03-20 11:36:36 +0900

    #22 [FIX/STYLE] 로그인 화면 버튼 및 헤더 hidden 수정
    #22 [FIX/STYLE] 로그인 화면 버튼 및 헤더 hidden 수정

--- src/index.js
+++ src/index.js
@@ -16,8 +16,10 @@ import GlobalStyle from './styles/GlobalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <>
     <GlobalStyle />
     <App />
+  </>
   // </React.StrictMode>
 );
 
