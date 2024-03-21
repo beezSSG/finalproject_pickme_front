2579989f9f4c783738dd5a1ccf4dbe047b71fd43
jisu <parangwavetraveller@gmail.com>
2024-03-21 09:33:05 +0900

    #22 feat: flowbite 설치
    flowbite 모듈 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: [
+            "./src/**/*.{js,jsx,ts,tsx}", 
+            "node_modules/flowbite-react/lib/esm/**/*.js",
+          ],
 
   theme: {
     screens: {
@@ -10,5 +13,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
