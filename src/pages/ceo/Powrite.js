222d9c7de784bd25b3c3e897d66066fb980d684a
wonbi Jeong <149459250+wonbi-jeong@users.noreply.github.com>
2024-03-22 13:25:17 +0900

    찐 마무리는 아님ㅠ

--- src/pages/ceo/Powrite.js
+++ src/pages/ceo/Powrite.js
@@ -1,41 +1,37 @@
-import { Component, useEffect, useState, createContext,useRef } from 'react';
-import { useNavigate } from 'react-router-dom'; // location.href
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { react } from '@babel/types';
-import { Checkbox } from '@mui/material';
-import React from 'react';
 import './powrite.css';
 
 const Powrite = () => {
-    const CheckboxContext = createContext();
+    const [categoryList, setCategoryList] = useState([]);
 
-    const [powrite, setPowrite] = useState([]);  
+    const [categoryName, setCategoryname] = useState([]);
+    const [productName, setProductName] = useState([]);
 
     const [modalOpen, setModalOpen] = useState(false);
-
     const modalBackground = useRef();
+
+    const [checkedList, setCheckedList] = useState([]);
+
 
     // 검색
     const [choice, setChoice] = useState("");
     const [search, setSearch] = useState("");
 
-    const getPowrite = (c,s) => {
-        axios.get("http://localhost:8080/powrite", 
-                    {params:{ choice:c, search:s }})
-                    .then(function(resp){  // success:function
-                        console.log(resp.data.powrite);
-                        setPowrite(resp.data);
-                     })
-                     .catch(function(err){     // error:function
-                        alert('error');
-             })
-             .catch((err)=>{
+    const getPowrite = () => {
+        axios.get("http://localhost:8080/powriteCn")
+            .then(function(resp){
+                console.log(resp.data); // 확인용
+                setCategoryList(resp.data); // 받아온 데이터로 카테고리 목록 업데이트
+                setCategoryname(resp.data.categoryName);
+            })
+            .catch(function(err){
                 alert('error');
-             })
+            })
     }
 
-    useEffect(function(){
-        getPowrite('', '');
+    useEffect(() => {
+        getPowrite();
     }, []);
 
     function searchBtn(){        
@@ -47,30 +43,18 @@ const Powrite = () => {
       getPowrite(choice, search);
     }  
 
-    const Product_list = [powrite.categoryName, powrite.name]
-
-    function Product() {    
-
-        const [checkedList, setCheckedList] = useState([]); // 데이터를 넣을 빈 배열
-
-        // onChange함수를 사용하여 이벤트 감지, 필요한 값 받아오기
-        const onCheckedElement = (checked, item) => {
-            if(checked) {
-                setCheckedList([...checkedList, item]); 
-            }else if(!checked){
+    // Checkbox가 선택되었을 때의 동작
+            const onCheckedElement = (checked, item) => {
+                if (checked) {
+                  setCheckedList([...checkedList, item]);
+                } else if (!checked) {
+                  setCheckedList(checkedList.filter(el => el !== item));
+                }
+              }; 
+              const onRemove = item => {
                 setCheckedList(checkedList.filter(el => el !== item));
-            }
-        };
-
-        // onChange함수를 사용하여 이벤트 감지, 필요한 값 받아오기
-        const onRemove = item => {
-            setCheckedList(checkedList.filter(el => el !== item));
-        };
-
-
-        
-    }
-
+              };
+            
     return(
         <div>
             <table style={{ marginLeft:"auto", marginRight:'auto', marginTop:"3px", marginBottom:"3px" }}>
@@ -78,7 +62,7 @@ const Powrite = () => {
                     <tr>
                         <td style={{ paddingLeft:"3px" }}>
                             <select className='custom-select' value={choice} onChange={(e)=>{setChoice(e.target.value)}}>
-                            <option value="name">상품명</option>
+                                <option value="name">상품명</option>
                             </select>
                         </td>
                         <td style={{ paddingLeft:"5px"}} className='align-middle'>
@@ -91,24 +75,13 @@ const Powrite = () => {
                     </tr>                
                 </tbody>    
             </table>
-            <h1>신청가능 품목</h1>
+            <h1>카테고리</h1>
     
-            {Product_list}<Checkbox value="" onClick={() => setModalOpen(true)}></Checkbox>
-            {
-                modalOpen &&
-                <div className={'modal-container'} ref={modalBackground} onClick={e => {
-                    if (e.target === modalBackground.current){
-                        setModalOpen(false);
-                    }
-                }}>
-                    <div className={'modal-content'}>
-                    <button className={'modal-close-btn'} onClick={() => setModalOpen(false)}>
-                        모달 닫기
-                    </button>
-                    </div>
-                </div>    
-            }
+            <div>
+
+            </div>
         </div>
     )
 }
+
 export default Powrite;
