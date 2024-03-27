95c063f2eb94bbf7e55dabd48cf4bf041d3d5346
jisu <parangwavetraveller@gmail.com>
2024-03-27 17:18:37 +0900

    #22 feat: tailwind에 색상 테마 변수를 넣기
    tailwind에 색상 테마 변수를 넣기

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
       md: { min: "820px", max: "1023px" },
       lg: { min: "1080px" },
     },
-    extend: {},
+    extend: {
+      colors: {
+        // 색상 옆에 어디 component에 쓰일 지 적어놓기 -> 통일된 디자인
+        'main-orange': '#ff6b00', //
+        'sub-orange': '#fca311',  //
+        'main-yellow': '#ffc300', // 
+        'sub-yellow': '#ffd60a', // 
+      },
+    },
   },
   plugins: [
   ],
