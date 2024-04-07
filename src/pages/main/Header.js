4262dd0945de24b822cd098879a60c985e837b65
jisu <parangwavetraveller@gmail.com>
2024-04-07 14:44:35 +0900

    #42 fix: navbar 코드 포맷팅
    navbar 코드 포맷팅

--- src/pages/main/Header.js
+++ src/pages/main/Header.js
@@ -31,8 +31,13 @@ function Header() {
           </Link>
 
           {/* 장바구니 */}
-          <Link to={ "http://localhost:3000/mypage" } className="group" onClick={()=>setMobileMenuOpen(false)}>
-            <span className="absolute py-1 px-1.5 w-max bg-transparent text-transparent translate duration-300 group-hover:bg-main-orange
+          <Link
+            to={"http://localhost:3000/mypage"}
+            className="group"
+            onClick={() => setMobileMenuOpen(false)}
+          >
+            <span
+              className="absolute py-1 px-1.5 w-max bg-transparent text-transparent translate duration-300 group-hover:bg-main-orange
                             text-xs group-hover:text-white font-medium rounded-xl sm:top-4 sm:right-12 md:top-6 md:right-2 lg:top-0 lg:right-2"
             >
               장바구니
@@ -62,11 +67,16 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
             to={() => logout()}
           >
             로그아웃
-          </Link>
+          </Link> */}
 
           {/* 마이페이지 */}
-          <Link to={ "http://localhost:3000/mypage" } className="group" onClick={()=>setMobileMenuOpen(false)}>
-            <span className="absolute py-1 px-1.5 w-max bg-transparent text-transparent translate duration-300 group-hover:bg-main-orange
+          <Link
+            to={"http://localhost:3000/mypage"}
+            className="group"
+            onClick={() => setMobileMenuOpen(false)}
+          >
+            <span
+              className="absolute py-1 px-1.5 w-max bg-transparent text-transparent translate duration-300 group-hover:bg-main-orange
                             text-xs group-hover:text-white font-medium rounded-xl sm:top-4 sm:right-[78px] md:top-6 md:right-9 lg:top-0 lg:right-9"
             >
               마이페이지
@@ -75,8 +85,13 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
           </Link>
 
           {/* 장바구니 */}
-          <Link to={ "http://localhost:3000/mypage" } className="group" onClick={()=>setMobileMenuOpen(false)}>
-            <span className="absolute py-1 px-1.5 w-max bg-transparent text-transparent translate duration-300 group-hover:bg-main-orange
+          <Link
+            to={"http://localhost:3000/mypage"}
+            className="group"
+            onClick={() => setMobileMenuOpen(false)}
+          >
+            <span
+              className="absolute py-1 px-1.5 w-max bg-transparent text-transparent translate duration-300 group-hover:bg-main-orange
                             text-xs group-hover:text-white font-medium rounded-xl sm:top-4 sm:right-12 md:top-6 md:right-2 lg:top-0 lg:right-2"
             >
               장바구니
@@ -102,7 +117,6 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
     { name: "고객센터", to: "/customercenter" },
   ];
 
-
   return (
     <div
       className={`mx-auto pt-6 pb-4 sm:p-0 lg:px-8 
@@ -117,7 +131,7 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
             src={FullLogoImg}
             alt="pickme full logo"
             className="sm:size-24 md:size-28"
-            onClick={()=>setMobileMenuOpen(false)}
+            onClick={() => setMobileMenuOpen(false)}
           />
           {/* <img
             src={ShortLogoImg}
@@ -125,7 +139,7 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
             className="sm:size-10 md:hidden lg:hidden"
           /> */}
         </Link>
-        
+
         {/* 메뉴 links */}
         <div className="sm:hidden">
           {menus.map((menu) => (
@@ -150,7 +164,7 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
         >
           <div className="md:block flex items-center my-1.5">
             <Logincom />
-            
+
             {/* <a className="inline-block rounded-lg px-1.5 py-1 text-base font-bold text-slate-500 md:text-sm hover:text-slate-900" href="/login">Sign in</a> 
             {adminName === "하기성" && (
                <Link className="inline-block rounded-lg px-1.5 py-1 text-base font-bold text-slate-500 md:text-sm hover:text-slate-900" to="/manager/orderchart">관리자</Link>
@@ -204,24 +218,25 @@ after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 afte
         </div>
       </nav>
 
-      <div className={`mt-6 z-20 h-screen w-screen transition-all duration-1000
-                    ${mobileMenuOpen ? "bg-white opacity-1" : "opacity-0 h-0"} md:hidden lg:hidden`}
+      <div
+        className={`mt-6 z-20 h-screen w-screen transition-all duration-1000
+                    ${
+                      mobileMenuOpen ? "bg-white opacity-1" : "opacity-0 h-0"
+                    } md:hidden lg:hidden`}
       >
         <div className="-my-8 pt-3 flex flex-col items-center">
           {menus.map((menu) => (
             <div className="flex flex-col items-center">
               <Link
-              key={menu.name}
-              to={menu.to}
-              onClick={()=>setMobileMenuOpen(false)}
-              className="inline-block text-base relative w-fit after:block peer
+                key={menu.name}
+                to={menu.to}
+                onClick={() => setMobileMenuOpen(false)}
+                className="inline-block text-base relative w-fit after:block peer
                       font-semibold text-slate-500 hover:text-slate-900 transition duration-300
                       after:content-[''] after:absolute after:h-[3px] after:-left-[20px] after:bg-main-yellow after:w-[112px]
                       after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
               >
-                <p className="my-4">
-                  {menu.name}
-                </p>
+                <p className="my-4">{menu.name}</p>
               </Link>
               <hr className="w-28 border-2 border-slate-200" />
             </div>
