e6d2e3fba1454c445749f7f1a1159f81d70c91cf
jisu <parangwavetraveller@gmail.com>
2024-04-10 15:07:08 +0900

    #55 fix: 매장찾기 코드 전체 수정 및 리팩토링
    기존 지도 생성과 마커 찍기를 담당하던 MyMap 컴포넌트를 삭제하고 useRef를 사용하여 보이는 지도 영역에만 마커를 찍을 수 있도록 대대적으로 수정함
    
    Co-Authored-By: Mingi Kang <37468959+KangGuide@users.noreply.github.com>

--- src/pages/store/StoreMap.js
+++ src/pages/store/StoreMap.js
@@ -1,12 +1,3 @@
-import {
-  NavermapsProvider,
-  Container as MapDiv,
-  NaverMap,
-  Marker,
-  useNavermaps,
-} from "react-naver-maps";
-// import './Navermap.css';
-
 import axios from "axios";
 
 // import MapData from "../../assets/data/store/emart24_busan.json";  // 프론트에 있던 기존 매장 data
@@ -15,119 +6,136 @@ import { useEffect, useRef, useState } from "react";
 import LeftMenu from "./LeftMenu/LeftMenu";
 
 export default function StoreMap() {
-  // const mapRef = useRef<NaverMap | null>(null);
-  const [mapdata, setMapdata] = useState();
-  const [loading, setLoading] = useState(false);
-  const [createMarkerList, setCreateMarkerList] = useState([]); //마커를 담을 배열
+  const mapRef = useRef(null);
+  const markersRef = useRef([]);
+  const [myLocation, setMyLocation] = useState();
+  // const [storesInMap, setStoresInMap] = useState([]);
+
+  const [userLatitude, setUserLatitude] = useState(0);
+  const [userLongtitude, setUserLongtitude] = useState(0);
+  const [userPoint, setUserPoint] = useState({});
+  // const [userPoint, setUserPoint] = useState({userLatitude, userLongtitude});
+
   useEffect(() => {
-    getMapData();
-    // createMarker();
-    console.log(createMarkerList);
+    // index.html에 script연결이 되어있는지 확인
+    if (!window.naver) {
+      console.error("Naver Maps API가 로드되지 않았습니다.");
+      return;
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setMyLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        })
+        // console.log("좌표는늘도망가..");
+      }, 
+      setMyLocation({
+        //  fallback
+        lat: 35.16591583,
+        lon: 129.1324683,
+      })
+    );
+  }
+
+    // 맵 생성조건 - 위치, 줌, 등등 건드리시면됩니다.
+    const mapOptions = {
+      center: new window.naver.maps.LatLng(35.16591583, 129.1324683),
+      zoom: 16,
+    };
+
+    // 지도 초기화
+    const map = new window.naver.maps.Map("map", mapOptions);
+    mapRef.current = map;
+
+    // 지도 로드시 꼭지점 위치 계산
+    getBoundsCorners(map);
+
+    // 지도의 바운드가 변경될 때마다 꼭지점의 위치 업데이트
+    window.naver.maps.Event.addListener(map, 'bounds_changed', ()=> { getBoundsCorners(map) });
+
+    // 마커 생성 및 마커 참조에 저장
+    // markersRef.current = sampleData.map((store) => new window.naver.maps.Marker({
+    //   map: map,
+    //   position: new window.naver.maps.LatLng(store.lat, store.lng),
+    // }));
+
+    // // 지도 영역 변경 시 마커 표시 업데이트 및 이벤트 딜레이주기
+    // const debouncedUpdateVisibleMarkers = debounce(
+    //   () => updateVisibleMarkers(map, markersRef.current),
+    //   1000
+    // ); // 1초 지연
+    // window.naver.maps.Event.addListener(
+    //   map,
+    //   "bounds_changed",
+    //   () => debouncedUpdateVisibleMarkers
+    // );
+
+    // return () => {
+    //   // 페이지가 들어왔다는 상태를 인지 -> 이 상태를 전역변수로 관리 -> 밑 코드가 실행가능
+    //   // 페이지에서 이동할 때 현재 페이지에 있는걸 clear -> 페이지 이동
+    //   // emptyCacheStorage();
+    // };
   }, []);
 
-  // // 지도 줌 인/아웃 시 마커 업데이트 이벤트 핸들러
-  // MyMap.Event.addListener(mapRef.current, "zoom_changed", () => {
-  //   if (mapRef.current !== null) {
-  //     updateMarkers(mapRef.current, Marker);
-  //   }
-  // });
-  // // 지도 드래그 시 마커 업데이트 이벤트 핸들러
-  // MyMap.maps.Event.addListener(mapRef.current, "dragend", () => {
-  //   if (mapRef.current !== null) {
-  //     updateMarkers(mapRef.current, Marker);
-  //   }
-  // });
+  // 꼭지점 구하고 화면 이동시 점포 위치 구하는것
+  function getBoundsCorners(map) {
+    const bounds = map.getBounds();
+    const sw = bounds.getSW(); // 남서쪽(South-West) 꼭지점
+    const ne = bounds.getNE(); // 북동쪽(North-East) 꼭지점
 
-  //반복문을 통해 데이터 배열 순회하면서 마커 생성 진행!
-  // function createMarker() {
-  //   for (let i = 0; i < Object.keys(mapdata).length; i++) {
-  //     let newmarker = new window.naver.maps.Marker({
-  //       position: new window.naver.maps.LatLng(mapdata[i].lat, mapdata[i].lon),
-  //     })
+    // 남동쪽(South-East)과 북서쪽(North-West) 꼭지점 계산
+    const nw = new window.naver.maps.LatLng(ne.lat(), sw.lng());
+    // const se = new window.naver.maps.LatLng(sw.lat(), ne.lng());
+    
+    // console.log("남서쪽(SW): ", sw);
+    // console.log("북동쪽(NE): ", ne);
+    // console.log("북서쪽(NW): ", nw);
 
-  //     setCreateMarkerList(prev => [...prev, newmarker]);
-  //   }
-  // }  
-
-  function getMapData() {
+    // axios로 매장의 값을 받아와야함
     axios
-      .get("http://localhost:8080/api/v1/store/storelist")
+      .get("/store/getstoresinmap", { params : { "swLat": sw._lat, "nwLat": nw._lat, "nwLng": nw._lng, "neLng": ne._lng } })
       .then((resp) => {
         // console.log(resp.data);
-        setMapdata(resp.data);
-        setLoading(true);
-        let abc = [];
-        for (let i = 0; i < Object.keys(resp.data).length; i++) {
-          let newmarker = new window.naver.maps.Marker({
-            position: new window.naver.maps.LatLng(mapdata[i].lat, mapdata[i].lon),
-          })
-
-          abc.push(newmarker);
-        }
-        setCreateMarkerList(prev => [...prev, abc]);
+        // setStoresInMap(resp.data);
+        const storeData = resp.data;
+        // console.log(storeData);
+        markersRef.current = storeData.map((store) => new window.naver.maps.Marker({
+        map: map,
+        position: new window.naver.maps.LatLng(store.lat, store.lon),
+        }));
       })
       .catch((err) => {
         alert(err);
       });
   }
 
-  function MyMap() {
-    // instead of window.naver.maps
-    const navermaps = useNavermaps();
+  // 보이는 영역에 따라 마커 표시 업데이트
+  const updateVisibleMarkers = (map, markers) => {
+    const mapBounds = map.getBounds();
+    markers.forEach((marker) => {
+      const isVisible = mapBounds.hasLatLng(marker.getPosition());
+      marker.setMap(isVisible ? map : null);
+    });
+  };
 
-    return (
-      <>
-        <NaverMap
-          // defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
-          // defaultCenter={new navermaps.LatLng(prop.location.latitude, prop.location.longitude)}
-          defaultCenter={new navermaps.LatLng(35.16591583, 129.1324683)}
-          defaultZoom={17}
-        >
-          <Marker
-            position={new navermaps.LatLng(35.16591583, 129.1324683)}
-            animation={1}
-            icon={{
-              content: `<button className="markerBox" style="font-size: 30px">
-                        🙋‍♂️
-                      </button>`,
-            }}
-          />
-          {/* {mapdata.map(function (mark, i) {
-            return (
-              <Marker
-                position={new navermaps.LatLng(mark.lat, mark.lon)}
-                animation={0}
-                icon={{
-                  url: MarkerImg, // 아이콘 경로
-                  size: new navermaps.Size(30, 40), //아이콘 크기
-                  origin: new navermaps.Point(0, 0),
-                  // anchor: new navermaps.Point(11, 35),
-                }}
-              />
-            );
-          })} */}
-        </NaverMap>
-      </>
-    );
-  }
-
-  if (loading === false) {
-    return <div>loading...</div>;
+  // 이벤트 발생에 딜레이를 걸어서 발동하게하는 함수
+  function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+      const context = this;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(context, args), wait);
+    };
   }
 
   return (
-    <>
-      <NavermapsProvider
-        ncpClientId="im12jhyvy5"
-        // or finClientId, govClientId
-      >
-        <MapDiv className="h-svh">
-          <LeftMenu props={mapdata} />
-          <MyMap />
-        </MapDiv>
-      </NavermapsProvider>
-      <br />
-      <br />
-    </>
+    // <div style={{ width: "100%" }}>
+    <div className="h-svh">
+      {/* LeftMenu에 prop로 값 넘겨주세요 */}
+      <LeftMenu props={markersRef} />
+      <div id="map" className="h-svh"></div>
+    </div>
   );
 }
