6699b6df42a7da5fe6e1cb6eac0563a2e32c71a5
jisu <parangwavetraveller@gmail.com>
2024-04-11 00:56:18 +0900

    #58 feat: 서비스 소개 페이지 기본 레이아웃
    서비스 소개 페이지 기본 레이아웃 잡기

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,9 +1,31 @@
+import RwIcon from "../../assets/imgs/about/rw.gif";
+import MgIcon from "../../assets/imgs/about/mg.gif";
+import JhIcon from "../../assets/imgs/about/jh.gif";
+import JsIcon from "../../assets/imgs/about/js.gif";
+import WbIcon from "../../assets/imgs/about/wb.gif";
+import KsIcon from "../../assets/imgs/about/ks.gif";
 
+const icons = [
+    { "name": " icon", "src": RwIcon },
+    { "name": " icon", "src": MgIcon },
+    { "name": " icon", "src": JhIcon },
+    { "name": " icon", "src": JsIcon },
+    { "name": " icon", "src": WbIcon },
+    { "name": " icon", "src": KsIcon },
+]
 
 export default function About() {
-    return(
-        <>
-        
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center justify-center px-auto">
+        <div className="lg:flex flex-wrap space-x-28">
+          {
+            icons.map((icon)=>(
+                <img key={ icon } src={ icon.src } alt={ icon.name } />
+            ))
+          }
+        </div>tr
+      </div>
+    </>
+  );
 }
