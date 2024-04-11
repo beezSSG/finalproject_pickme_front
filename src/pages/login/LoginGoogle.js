dd5818c133cee46d8ccfda36208a683e6203492f
rog-won <looky1254@naver.com>
2024-04-11 15:55:00 +0900

    # 자동화 테스트 sns 로그인 url바꿈
    # 자동화 테스트 sns 로그인 url바꿈

--- src/pages/login/LoginGoogle.js
+++ src/pages/login/LoginGoogle.js
@@ -6,7 +6,7 @@ const LoginGoogle = (props) => {
     useEffect(() => {
         const googleLogin = async () =>{
             await axios
-                .get( "http://localhost:8080/login/oauth2/code/google?code="+code)
+                .get( "http://mypickme.pickme-ssg.com/login/oauth2/code/google?code="+code)
                 .then((res) => {
                     localStorage.setItem("id", res.data.id);
                     localStorage.setItem("name", res.data.name);
@@ -14,7 +14,7 @@ const LoginGoogle = (props) => {
                     
                     //네이게이트 사용했을 때 바로 스토리지 값 랜더링 안되서 그냥 window.location.replace 사용
                     // navigate("/");
-                    window.location.replace('http://localhost:3000/');
+                    window.location.replace('http://mypickme.pickme-ssg.com/');
                 })
                 .catch((e) => {
                     alert("error");
