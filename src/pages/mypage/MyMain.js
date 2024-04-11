22a5468d63d9315e47132956109d32c4294d69c8
Mingi Kang <gi3592@gmail.com>
2024-04-11 14:27:31 +0900

    #64 마이페이지 수정
    #64 마이페이지 수정

--- src/pages/mypage/MyMain.js
+++ src/pages/mypage/MyMain.js
@@ -49,7 +49,7 @@ export default function MyMain() {
   const topBar = [
     { title: "장바구니", path: "/mypage/cart" },
     { title: "포인트", path: "/mypage/point" },
-    { title: "사용가능 쿠폰", path: "/mypage/coupon" },
+    { title: "쿠폰", path: "/mypage/coupon" },
     { title: "찜 목록", path: "/mypage/save" },
     { title: "선물함", path: "/mypage/gift" },
   ];
@@ -60,8 +60,8 @@ export default function MyMain() {
 
   return (
     <>
-      <div className="grid grid-cols-6 w-full mb-10 sm:flex-wrap md:grid-cols-2">
-        <div className="pl-4 pt-5">
+      <div className="lg:flex w-full mb-10 md:grid-cols-2 mt-5">
+        <div className="pl-4 mr-24">
           <div>
             <span className="text-4xl font-bold  text-black-500">
               {info.name}
@@ -70,27 +70,29 @@ export default function MyMain() {
               {info.grade}
             </span>
           </div>
-          <div className="mt-2 text-sm font-bold text-neutral-500">
-            <Link to="/mypage/userinfo" className="hover:text-sub-yellow">
-              회원정보수정
-            </Link>
+          <div className="flex mt-2 text-sm font-bold text-neutral-500">
+            <Link to="/mypage/userinfo" >회원정보수정</Link>
+            <button>메뉴창 보기</button>
           </div>
         </div>
 
-        
+        <div className="grid grid-cols-5 sm:grid-cols-3 sm:gap-5 sm:font-thin text-[0.5rem] sm:h-5">
+
         { 
           topBar.map((topbar, i) => (
-          <div className="pl-[10px] pr-[10px] pt-5 pb-5 font-bold rounded-3xl shadow-xl bg-stone-100 mx-5" key={i}>
+          <div className="w-[90%] lg:p-5 lg:mx-5 sm:p-1 sm:mx-1 font-bold rounded-3xl shadow-xl bg-stone-100 " key={i}>
             <button className="w-full" onClick={ () => {window.location.href = `${topbar.path}`} }>
-              <div className="text-left text-2xl text-neutral-500">{topbar.title}</div>
+              <div className="text-left lg:text-2xl text-neutral-500">{topbar.title}</div>
               { i === 1 ?
-              <div className="text-right mt-4 text-4xl text-yellow-600 ">{topInfo[i].toLocaleString()}원</div>
+              <div className="text-right lg:mt-4 lg:text-4xl text-yellow-600 ">{topInfo[i].toLocaleString()}P</div>
               :
-              <div className="text-right mt-4 text-4xl text-yellow-600 ">{topInfo[i]}개</div>
+              <div className="text-right lg:mt-4 lg:text-4xl text-yellow-600 ">{topInfo[i]}개</div>
               }
             </button>
           </div>
-        ))}
+          ))
+        }
+        </div>
       </div>
 
       <div className="flex w-full">
