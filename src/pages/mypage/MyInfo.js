983e19a3d4d54b65368e5e18c5e216a8511447d5
hakiseong <ks510222@naver.com>
2024-04-11 09:19:27 +0900

    #50 fix myinfo
    #50 fix myinfo

--- src/pages/mypage/MyInfo.js
+++ src/pages/mypage/MyInfo.js
@@ -5,17 +5,17 @@ import { Button, Modal } from "antd";
 
 export default function MyInfo() {
   const [userInfo, setUserInfo] = useState({});
-  
+
   // 개인정보 수정
   const [changeInfo, setChangeInfo] = useState(false);
   const [address, setAddress] = useState({}); // 주소
-  const [phoneNumber, setPhoneNumber] = useState('') // 전화번호
+  const [phoneNumber, setPhoneNumber] = useState(""); // 전화번호
 
   // 비밀번호 수정
   const [changePw, setChangePw] = useState(false);
-  const [myPw, setMyPw] = useState(0);  // 원래 비밀번호
-  const [newPw, setNewPw] = useState(0); // 변경 비밀번호
-  const [confirmPw, setConfirmPw] = useState(0); // 비밀번호 확인
+  const [myPw, setMyPw] = useState(""); // 원래 비밀번호
+  const [newPw, setNewPw] = useState(""); // 변경 비밀번호
+  const [confirmPw, setConfirmPw] = useState(""); // 비밀번호 확인
 
   // 회원탈퇴
   const [open, setOpen] = useState(false);
@@ -26,22 +26,25 @@ export default function MyInfo() {
 
   // Axios 호출 [이름, 등급(영문으로변경), 장바구니 수량, 포인트, 쿠폰, 찜 목록, 선물함]
   const getInfo = async () => {
-    await axios.get("mypage/user/getUserInfo")
-    .then((response)=>{
-      // console.log(JSON.stringify(response.data));
-      setUserInfo(response.data);
-    })
-    .catch((err)=>{
-      alert(err);
-    })
-  }
+    await axios
+      .get("mypage/user/getUserInfo")
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        setUserInfo(response.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
 
   // 값 체인지 핸들러
   function chageHandler(i) {
     // console.log(i);
-    if (i === 0) { // 개인정보 변경
+    if (i === 0) {
+      // 개인정보 변경
       setChangeInfo(!changeInfo);
-    } else if (i === 1) { // 비밀번호 변경
+    } else if (i === 1) {
+      // 비밀번호 변경
       setChangePw(!changePw);
     }
   }
@@ -53,18 +56,14 @@ export default function MyInfo() {
 
   // 전화번호만 가능하게 만들기
   const parsingPhoneNumber = (num) => {
-    return (
-        num
-            .replace(/[^0-9]/g, '')
-            .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3')
-            .replace(/(-{1,2})$/g, '')
-    )
-  }
+    return num
+      .replace(/[^0-9]/g, "")
+      .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
+      .replace(/(-{1,2})$/g, "");
+  };
 
   // 개인정보 변경
-  function changeMyInformation() {
-    
-  }
+  function changeMyInformation() {}
 
   // 비밀번호 변경
 
@@ -81,155 +80,256 @@ export default function MyInfo() {
     // }
   }
 
-
-
-
   return (
-    <div className="ml-14 w-[70%]">
-      <div className="flex flex-row mt-5">
-        <div className="w-[15%]">
-          <p className="text-4xl font-bold ">개인정보</p>
+    <div className="w-[70%] mx-auto">
+      {/* Personal Information Section */}
+      <div className="flex gap-40">
+        <div className="w-[20%]">
+          <p className="text-3xl font-bold ">개인정보</p>
         </div>
-        <div className="w-[85%] pl-5">
-          <div className="flex text-2xl sm:text-sm">
-            <span className="w-24">아이디 </span>
-            <div className="ml-2">{userInfo.email}</div>
+        <div className="w-[80%] pl-5">
+          {/* Individual Info Blocks */}
+          <div className="flex gap-12 mb-2">
+            <span className="w-24 text-xl font-bold text-gray-600">
+              아이디{" "}
+            </span>
+            <div className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60">
+              {userInfo.email}
+            </div>
           </div>
-          <div className="flex mt-3 text-2xl">
-            <span className="w-24">이름 </span>
-            <div className="ml-2">{userInfo.name}</div>
+          <div className="flex gap-12 mb-2">
+            <span className="w-24 text-xl font-bold text-gray-600">이름 </span>
+            <div className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60">
+              {userInfo.name}
+            </div>
           </div>
-          <div className="mt-3 text-2xl">
-            {changeInfo && <span className="w-24">주소 </span>}
-            {changeInfo ? 
-              <div className="flex mt-3">
-                <input type='text' placeholder="주소" readOnly="true" className="w-96 rounded-lg border-slate-200 border-2 text-slate-600 transition duration-500 bg-transparent hover:border-sub-yellow focus:border-sub-yellow focus:outline-none" value={address.address}/>
+          <div className="flex gap-12 mb-2">
+            {changeInfo && (
+              <span className="w-24 text-xl font-bold text-gray-600">
+                주소{" "}
+              </span>
+            )}
+            {changeInfo ? (
+              <div className="flex gap-3 mb-2">
+                <input
+                  type="text"
+                  placeholder="주소"
+                  readOnly="true"
+                  className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60 focus:border-sub-yellow focus:outline-none"
+                  value={address.address}
+                />
                 <MyInfoPost updateAddress={onSetAddress} />
               </div>
-              :
-              <div className="flex">
-                <span className="w-24">주소 </span>
-                <div className="ml-2">{userInfo.address}</div>
+            ) : (
+              <div className="flex gap-12 mb-2">
+                <span className="w-24 text-xl font-bold text-gray-600">
+                  주소{" "}
+                </span>
+                <div className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60">
+                  {userInfo.address}
+                </div>
               </div>
-            }
+            )}
           </div>
-          <div className="flex mt-3 text-2xl">
-            <span className="w-24">전화번호 </span>
-            {changeInfo ? 
-              <input type='tel' placeholder="휴대폰 번호를 입력해주세요" maxLength={13} value={phoneNumber} onChange={(e) => setPhoneNumber(parsingPhoneNumber(e.target.value))} 
-                className="ml-2 w-96  rounded-lg border-slate-200 border-2 text-slate-600 transition duration-500 bg-transparent hover:border-sub-yellow focus:border-sub-yellow focus:outline-none" />
-              :
-              <div className="ml-2">{userInfo.phone}</div>
-            }
+          <div className="flex gap-12 mb-2">
+            <span className="w-24 text-xl font-bold text-gray-600">
+              전화번호{" "}
+            </span>
+            {changeInfo ? (
+              <input
+                type="tel"
+                placeholder="휴대폰 번호를 입력해주세요"
+                maxLength={13}
+                value={phoneNumber}
+                onChange={(e) =>
+                  setPhoneNumber(parsingPhoneNumber(e.target.value))
+                }
+                className="text-lg border-2 border-gray-500 rounded-xl p-2 w-72 focus:border-sub-yellow focus:outline-none"
+              />
+            ) : (
+              <div className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60">
+                {userInfo.phone}
+              </div>
+            )}
           </div>
-          <div className="flex mt-3 text-2xl">
-            <span className="w-24">등급 </span>
-            <div className="ml-2">{userInfo.grade}</div>
+          <div className="flex gap-12 mb-2">
+            <span className="w-24 text-xl font-bold text-gray-600">등급 </span>
+            <div className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60">
+              {userInfo.grade}
+            </div>
           </div>
-          <div className="flex mt-3 text-2xl">
-            <span className="w-24">가입일 </span>
-            <div className="ml-2">{userInfo.rdate}</div>
+          <div className="flex gap-12 mb-2">
+            <span className="w-24 text-xl font-bold text-gray-600">
+              가입일{" "}
+            </span>
+            <div className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60">
+              {userInfo.rdate}
+            </div>
           </div>
           <div className="mt-3">
-            { changeInfo ?
-              <div>
-                <button type="button" className="border-2 border-orange-500 bg-orange-500 text-white" onClick={chageHandler}>
-                확인
+            {changeInfo ? (
+              <div className="text-right">
+                <button
+                  type="button"
+                  className="bg-sub-yellow rounded-xl p-2 font-bold w-20 hover:bg-sub-orange"
+                  onClick={() => chageHandler(0)}
+                >
+                  확인
                 </button>
-                <button type="button" className="ml-5 border-2 border-orange-500 bg-orange-500 text-white" onClick={() => {chageHandler(0)}}>
-                취소
+                <button
+                  type="button"
+                  className="ml-5 bg-gray-300 rounded-xl p-2 font-bold w-20 hover:bg-gray-500"
+                  onClick={() => chageHandler(0)}
+                >
+                  취소
                 </button>
               </div>
-              :
-              <button type="button" className="border-2 border-orange-500 bg-orange-500 text-white" onClick={() => {chageHandler(0)}}>
-              변경
-              </button>
-            }
+            ) : (
+              <div className="text-right">
+                <button
+                  type="button"
+                  className="bg-sub-yellow rounded-xl p-2 font-bold w-20 hover:bg-sub-orange"
+                  onClick={() => chageHandler(0)}
+                >
+                  변경
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
 
-      <div className="flex flex-row border-t-2 mt-5">
-        <div className="w-[15%] mt-5">
-          <p className="text-4xl font-bold">비밀번호 변경</p>
+      {/* Password Change Section */}
+      <div className="flex gap-40">
+        <div className="w-[20%] mt-5">
+          <p className="text-3xl font-bold">비밀번호 변경</p>
         </div>
-        <div className="w-[85%] mt-5 pl-5">
-          <div className="flex text-2xl">
-            <div className="w-48">현재 비밀번호 </div>
-            { changePw ? <input type="password" className="w-56 rounded-lg border-slate-200 border-2 text-base text-slate-600 transition duration-500 bg-transparent hover:border-sub-yellow focus:border-sub-yellow focus:outline-none" /> : "" }
+        <div className="w-[80%] mt-5 pl-5">
+          {/* Password Change Fields */}
+          <div className="flex gap-12 mb-2">
+            <span className="w-48 text-xl font-bold text-gray-600">
+              현재 비밀번호
+            </span>
+            <input
+              type="password"
+              placeholder="현재 비밀번호"
+              className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60 focus:border-sub-yellow focus:outline-none"
+              value={myPw}
+              onChange={(e) => setMyPw(e.target.value)}
+            />
           </div>
-          <div className="flex text-2xl mt-3">
-            <div className="w-48">새로운 비밀번호 </div>
-            { changePw ? <input type="password" className="w-56 rounded-lg border-slate-200 border-2 text-base text-slate-600 transition duration-500 bg-transparent hover:border-sub-yellow focus:border-sub-yellow focus:outline-none" /> : "" }
+          <div className="flex gap-12 mb-2">
+            <span className="w-48 text-xl font-bold text-gray-600">
+              변경할 비밀번호{" "}
+            </span>
+            <input
+              type="password"
+              placeholder="새 비밀번호"
+              className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60 focus:border-sub-yellow focus:outline-none"
+              value={newPw}
+              onChange={(e) => setNewPw(e.target.value)}
+            />
           </div>
-          <div className="flex text-2xl mt-3">
-            <div className="w-48">비밀번호 확인 </div>
-            { changePw ? <input type="password" className="w-56 rounded-lg border-slate-200 border-2 text-base text-slate-600 transition duration-500 bg-transparent hover:border-sub-yellow focus:border-sub-yellow focus:outline-none" /> : "" }
+          <div className="flex gap-12 mb-2">
+            <span className="w-48 text-xl font-bold text-gray-600">
+              비밀번호 확인{" "}
+            </span>
+            <input
+              type="password"
+              placeholder="비밀번호 확인"
+              className="text-lg border-2 border-gray-500 rounded-xl p-2 w-60 focus:border-sub-yellow focus:outline-none"
+              value={confirmPw}
+              onChange={(e) => setConfirmPw(e.target.value)}
+            />
           </div>
-          <div className="">
-            { changePw ?
-              <div className="mt-3">
-                <button type="button" className="border-2 border-orange-500 bg-orange-500 text-white" onClick={chageHandler}>
-                확인
+          <div className="mt-3">
+            {changePw ? (
+              <div className="text-right">
+                <button
+                  type="button"
+                  className="bg-sub-yellow rounded-xl p-2 font-bold w-20 hover:bg-sub-orange"
+                  onClick={() => chageHandler(1)}
+                >
+                  확인
                 </button>
-                <button type="button" className="ml-5 border-2 border-orange-500 bg-orange-500 text-white" onClick={() => {chageHandler(1)}}>
-                취소
+                <button
+                  type="button"
+                  className="ml-5 bg-gray-300 rounded-xl p-2 font-bold w-20 hover:bg-gray-500"
+                  onClick={() => chageHandler(1)}
+                >
+                  취소
                 </button>
               </div>
-              :
-              <button type="button" className="border-2 border-orange-500 bg-orange-500 text-white" onClick={() => {chageHandler(1)}}>
-              변경
-              </button>
-            }
+            ) : (
+              <div className="text-right">
+                <button
+                  type="button"
+                  className="bg-sub-yellow rounded-xl p-2 font-bold w-20 hover:bg-sub-orange"
+                  onClick={() => chageHandler(1)}
+                >
+                  변경
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
-
-      <div className="flex flex-row border-t-2 mt-5">
-        <div className="w-[15%] mt-5">
-          <p className="text-4xl font-bold">Pick ME 탈퇴</p>
+      <div className="flex gap-40">
+        <div className="w-[20%] mt-5">
+          <p className="text-3xl font-bold">Pick ME 탈퇴</p>
         </div>
-        <div className="w-[85%] mt-5 pl-5">
-          <div className="">
-            <button type="button" className="border-2 border-orange-500 bg-orange-500 text-white" onClick={() => {onToggleModal()}}>
+        <div className="w-[80%] mt-5 pl-5">
+          <div>
+            <button
+              type="button"
+              className="bg-sub-yellow rounded-xl p-2 font-bold w-20 hover:bg-sub-orange "
+              onClick={() => {
+                onToggleModal();
+              }}
+            >
               회원탈퇴
             </button>
             {open && (
-            <Modal
-              open={true}
-              okText="회원탈퇴"
-              cancelText="취소"
-              onOk={onToggleModal}
-              onCancel={onToggleModal}
-              footer={null}
-            >
-              <div className="text-center">
-                <br />
-                <br />
-                <h1 className="text-red-500 font-bold text-2xl">경고문</h1>
-                <br />
-                <h1 className="text-lg">회원을 탈퇴하시면 지금까지의 모든 기록이 사라지고 </h1>
-                <h1 className="text-lg font-bold text-red-600">복구가 불가능합니다.</h1>
-                <br/>
-                
-                <h1 className="text-lg">정말로 회원탈퇴를 진행하시겠습니까?</h1>
-                <br />
-                <div className="grid grid-cols-2 gap-5"> 
-                  {/* 버튼을 중앙으로 정렬하기 위한 컨테이너 */}
-                  <Button key="back" onClick={onToggleModal}>
-                    떠날래요
-                  </Button>
-                  <Button key="submit" type="primary" onClick={onToggleModal}>
-                    더 써볼래요
-                  </Button>
+              <Modal
+                open={true}
+                okText="회원탈퇴"
+                cancelText="취소"
+                onOk={onToggleModal}
+                onCancel={onToggleModal}
+                footer={null}
+              >
+                <div className="text-center">
+                  <br />
+                  <br />
+                  <h1 className="text-red-500 font-bold text-2xl">경고문</h1>
+                  <br />
+                  <h1 className="text-lg">
+                    회원을 탈퇴하시면 지금까지의 모든 기록이 사라지고{" "}
+                  </h1>
+                  <h1 className="text-lg font-bold text-red-600">
+                    복구가 불가능합니다.
+                  </h1>
+                  <br />
+
+                  <h1 className="text-lg">
+                    정말로 회원탈퇴를 진행하시겠습니까?
+                  </h1>
+                  <br />
+                  <div className="grid grid-cols-2 gap-5">
+                    {/* 버튼을 중앙으로 정렬하기 위한 컨테이너 */}
+                    <Button key="back" onClick={onToggleModal}>
+                      떠날래요
+                    </Button>
+                    <Button key="submit" type="primary" onClick={onToggleModal}>
+                      더 써볼래요
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            </Modal>
-          )}
+              </Modal>
+            )}
           </div>
         </div>
       </div>
-
     </div>
   );
 }
--- src/pages/mypage/MyMain.js
+++ src/pages/mypage/MyMain.js
@@ -16,35 +16,35 @@ import MyGiftdetail from "./MyGiftdetail";
 export default function MyMain() {
   // useState 선언
   const navigater = useNavigate();
-  const {token} = useAuth();
+  const { token } = useAuth();
   const [info, setInfo] = useState();
   const [topInfo, setTopInfo] = useState([]); // 배열크기는 8개지만 필요한 정보를 앞에 배치하여 5개만 나오게하여 활용중
-  
 
   // 진입전 토큰 유무 확인 토큰 유효성시간을 대폭 상향
   useEffect(() => {
     if (token === null || token === undefined) {
-      alert('로그인이 필요한 서비스 입니다.');
-      navigater('/login');
+      alert("로그인이 필요한 서비스 입니다.");
+      navigater("/login");
     }
     getMyInfo();
   }, []);
 
   // Axios 호출 [이름, 등급(영문으로변경), 장바구니 수량, 포인트, 쿠폰, 찜 목록, 선물함]
   const getMyInfo = async () => {
-    await axios.get("http://localhost:8080/api/v1/mypage/getMyInfo", {
-      headers : { Authorization: `Bearer ${localStorage.getItem('jwt')}` }
-    })
-    .then((response)=>{
-      //console.log(JSON.stringify(response.data));
-      // console.log(Object.values(response.data));
-      setInfo(response.data);
-      setTopInfo(Object.values(response.data));
-    })
-    .catch((err)=>{
-      // alert(err);
-    })
-  }
+    await axios
+      .get("http://localhost:8080/api/v1/mypage/getMyInfo", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
+      })
+      .then((response) => {
+        //console.log(JSON.stringify(response.data));
+        // console.log(Object.values(response.data));
+        setInfo(response.data);
+        setTopInfo(Object.values(response.data));
+      })
+      .catch((err) => {
+        // alert(err);
+      });
+  };
 
   const topBar = [
     { title: "장바구니", path: "/mypage/cart" },
@@ -55,7 +55,7 @@ export default function MyMain() {
   ];
 
   if (info === undefined || info === null) {
-    return <div>loding...</div>
+    return <div>loding...</div>;
   }
 
   return (
@@ -63,41 +63,56 @@ export default function MyMain() {
       <div className="grid grid-cols-6 w-full mb-10 sm:flex-wrap md:grid-cols-2">
         <div className="pl-4 pt-5">
           <div>
-            <span className="text-4xl font-bold  text-black-500">{info.name}</span>
-            <span className="pl-2 text-3xl font-bold text-yellow-500">{info.grade}</span>
+            <span className="text-4xl font-bold  text-black-500">
+              {info.name}
+            </span>
+            <span className="pl-2 text-3xl font-bold text-yellow-500">
+              {info.grade}
+            </span>
           </div>
           <div className="mt-2 text-sm font-bold text-neutral-500">
-            <Link to="/mypage/userinfo" >회원정보수정</Link>
+            <Link to="/mypage/userinfo" className="hover:text-sub-yellow">
+              회원정보수정
+            </Link>
           </div>
         </div>
-        
-        { 
-          topBar.map((topbar, i) => (
-          <div className="pl-[10px] pr-[10px] pt-5 pb-5 font-bold rounded-3xl shadow-xl bg-stone-100 mx-5" key={i}>
-            <button className="w-full" onClick={ () => {window.location.href = `${topbar.path}`} }>
-              <div className="text-left text-2xl text-neutral-500">{topbar.title}</div>
-              <div className="text-right mt-4 text-4xl text-yellow-600 ">{topInfo[i]}</div>
+
+        {topBar.map((topbar, i) => (
+          <div
+            className="pl-[10px] pr-[10px] pt-5 pb-5 font-bold rounded-3xl shadow-xl bg-stone-100 mx-5"
+            key={i}
+          >
+            <button
+              className="w-full"
+              onClick={() => {
+                window.location.href = `${topbar.path}`;
+              }}
+            >
+              <div className="text-left text-2xl text-neutral-500">
+                {topbar.title}
+              </div>
+              <div className="text-right mt-4 text-4xl text-yellow-600 ">
+                {topInfo[i]}
+              </div>
             </button>
           </div>
-          ))
-        }
+        ))}
       </div>
-      
+
       <div className="flex w-full">
         <MyMainNav />
         <Routes>
-          <Route path='' element={<MyMainContent />} />
-          <Route path='cart' element={<MyCart point={topInfo[1]} />} />
-          <Route path='payinfo' element={<MyPayinfo />} />
-          <Route path='save' element={<MySave />} />
-          <Route path='userinfo' element={<MyInfo />} />
-          <Route path='review' element={<MyReview />} />
-          <Route path='coupon' element={<MyCoupon /> } />
-          <Route path='gift' element={<MyGift /> } />
-          <Route path='giftdetail' element={<MyGiftdetail /> } />
+          <Route path="" element={<MyMainContent />} />
+          <Route path="cart" element={<MyCart point={topInfo[1]} />} />
+          <Route path="payinfo" element={<MyPayinfo />} />
+          <Route path="save" element={<MySave />} />
+          <Route path="userinfo" element={<MyInfo />} />
+          <Route path="review" element={<MyReview />} />
+          <Route path="coupon" element={<MyCoupon />} />
+          <Route path="gift" element={<MyGift />} />
+          <Route path="giftdetail" element={<MyGiftdetail />} />
         </Routes>
       </div>
     </>
   );
 }
-
