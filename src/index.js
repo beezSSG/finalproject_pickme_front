8610995465663a05b1c7df3d2bf27e6b96321ccf
hakiseong <ks510222@naver.com>
2024-04-12 12:59:33 +0900

    #50 fix styledcomponent 모두 제거
    #50 fix styledcomponent 모두 제거

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 // tailwind css
 import "./styles/tailwindcss/input.css";
-import './styles/tailwindcss/output.css';  
+import "./styles/tailwindcss/output.css";
 
-// 전역 스타일
-import GlobalStyle from './styles/GlobalStyle';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-  <GlobalStyle />
-  <App />
+    <App />
   </>
 );
-
 
 // ServiceWorkerRegistration.unregister();  // unregister(); => register(); 로 고치셔야합니다.
 
