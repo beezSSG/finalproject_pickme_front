9e1913a5bf2173c78eb2660b9b167916246876a4
Mingi Kang <gi3592@gmail.com>
2024-04-14 01:07:04 +0900

    #72 Fix 아이콘 이미지 수정
    #72 Fix 아이콘 이미지 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import "./service-worker.js"
+
 
 // tailwind css
 import "./styles/tailwindcss/input.css";
@@ -17,15 +17,6 @@ root.render(
 );
 
 // Service Worker 등록
-// ServiceWorkerRegistration.register();
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker.register("/service-worker.js").then(registration => {
-//       console.log("Service Worker registered!");
-//     }).catch(error => {
-//       console.error("Service Worker registration failed:", error);
-//     });
-//   });
-// }
+// ServiceWorkerRegistration.register(); 
 
 reportWebVitals();
