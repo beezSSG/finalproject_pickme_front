f68da6f5487224d89c671b6ecf5b1a2d5e8d6177
jisu <parangwavetraveller@gmail.com>
2024-04-14 15:14:11 +0900

    #58 feat: 멤버 아이콘 배열 수정
    멤버 아이콘 배열 수정

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,3 +1,5 @@
+
+// 아이콘
 import RwIcon from "../../assets/imgs/about/rw.gif";
 import MgIcon from "../../assets/imgs/about/mg.gif";
 import JhIcon from "../../assets/imgs/about/jh.gif";
@@ -5,14 +7,53 @@ import JsIcon from "../../assets/imgs/about/js.gif";
 import WbIcon from "../../assets/imgs/about/wb.gif";
 import KsIcon from "../../assets/imgs/about/ks.gif";
 
+
+import { Link } from "react-router-dom";
+
 const icons = [
-    { "name": " icon", "src": RwIcon },
-    { "name": " icon", "src": MgIcon },
-    { "name": " icon", "src": JhIcon },
-    { "name": " icon", "src": JsIcon },
-    { "name": " icon", "src": WbIcon },
-    { "name": " icon", "src": KsIcon },
-]
+  {
+    name: "팀장 이록원",
+    development: "CI/CD, 메인홈",
+    src: RwIcon,
+    msg: "",
+    githubUrl: "https://github.com/rog-won",
+  },
+  {
+    name: "강민기",
+    development: "JWT, PWA구현, 마이페이지, 장바구니",
+    src: MgIcon,
+    msg: "",
+    githubUrl: "https://github.com/KangGuide",
+  },
+  {
+    name: "권지호",
+    development: "상품 페이지, ",
+    src: JhIcon,
+    msg: "",
+    githubUrl: "https://github.com/GONZOZO",
+  },
+  {
+    name: "오지수",
+    development: "메인홈, 서비스 소개, 매장찾기",
+    src: JsIcon,
+    msg: "",
+    githubUrl: "https://github.com/parangwave",
+  },
+  {
+    name: "정원비",
+    development: "점주",
+    src: WbIcon,
+    msg: "",
+    githubUrl: "https://github.com/wonbi-jeong",
+  },
+  {
+    name: "하기성",
+    development: "관리자, 픽업/배달, ",
+    src: KsIcon,
+    msg: "",
+    githubUrl: "https://github.com/hakiseong",
+  },
+];
 
 export default function About() {
   return (
