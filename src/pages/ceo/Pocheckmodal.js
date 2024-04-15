122b2b48e3b6b79c488e1bf4defad607ce2e74f1
wonbi Jeong <totoyo693@naver.com>
2024-04-15 16:33:07 +0900

    #74 hotfix 모듈 삭제했습니다
    #74 hotfix 모듈 삭제했습니다

--- src/pages/ceo/Pocheckmodal.js
+++ src/pages/ceo/Pocheckmodal.js
@@ -1,7 +1,5 @@
 import { useState, useRef } from "react";
 import { Modal, Button } from "antd";
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
 
 export default function Pocheckmodal() {
