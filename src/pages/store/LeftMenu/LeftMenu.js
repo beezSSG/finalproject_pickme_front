949fe305b7c0f1bc67c8fa297847841f9a02c9c3
jisu <parangwavetraveller@gmail.com>
2024-04-17 06:02:55 +0900

    #78 fix: 주석문 삭제 및 수정
    주석문 삭제 및 수정

--- src/pages/store/LeftMenu/LeftMenu.js
+++ src/pages/store/LeftMenu/LeftMenu.js
@@ -14,16 +14,13 @@ import LocSelect from "./LocSelect";
 import SearchStoreName from "./SearchStoreName";
 import StoreCategories from "./StoreCategories.js";
 
-
 // ({abc, bcd})
-export default function LeftMenu({storelist}) {
+export default function LeftMenu({ storelist }) {
   // console.log(props);
   const [loading, setLoading] = useState(false);
   const [menuOpen, setMenuOpen] = useState(true); // 왼쪽바 메뉴 열림 상태
   const [filterOpen, setFilterOpen] = useState(false); // 매장 검색 및 필터 열림 상태
 
-  const [targetStore, setTargetStore] = useState(""); // 검색할 매장명
-  // 임시로 array slice함
   const [initialStores, setInitialStores] = useState(); // 초기 렌더링 시 매장들
   const [stores, setStores] = useState(storelist); // 보여질 매장 목록
 
@@ -33,34 +30,26 @@ export default function LeftMenu({storelist}) {
   const [filter, setFilter] = useState([]);
 
   function setStorelist(data) {
-    console.log("부모에게 도달!");
+    // console.log("부모에게 도달!");
     setStores(data);
   }
-  // useEffect(()=>{
-  //   // console.log(stores);
-  //   setStores(props.stores);
-  //   // setLoading(true);
-
-  //   // - 지금 뭔가 워링이 무제한으로 걸리고 있다 useeffect 관련
-  //   // - 컴포넌트에 대한 방법을 해보니 일정 부분의 값이 고정되고 나머지가 리렌더링이 되고 있는 부분이 있다
-  // }, [])
 
   function setOneState(data) {
-    console.log("부모에게 도달!");
+    // console.log("부모에게 도달!");
     setState(data);
-    console.log(state);
+    // console.log(state);
   }
 
   function setOneDistrict(data) {
-    console.log("부모에게 도달!");
+    // console.log("부모에게 도달!");
     setDistrict(data);
-    console.log(district);
+    // console.log(district);
   }
 
   function setChosenCategories(data) {
     console.log("부모에게 도달!");
     setFilter(data);
-    console.log("filter: " + filter);
+    // console.log("filter: " + filter);
   }
 
   useEffect(() => {
@@ -70,52 +59,27 @@ export default function LeftMenu({storelist}) {
     if (initialStores !== undefined) {
       setLoading(true);
     } else {
-      // console.log(stores);
+      console.log(stores);
     }
-    
+
     if (filter !== undefined || filter.length !== 0) {
-      let filteredStores = initialStores;
-      filter.map((f)=>{
-        console.log(f);
-        // key이름이 겹침 -> 키에 대한 정확한 위치를 파악 && 거기에 각각 1이 맞는지 확인 작업
-        filteredStores = filteredStores.filter((item) => item.hasOwnProperty(f) && item[f] === 1) 
-      })
+      let filteredStores = initialStores; // 필터링한 매장 목록 변수 선언
+      filter.map((f) => {
+        // console.log(f);
+        // key 이름이 겹침 -> 키에 대한 정확한 위치를 파악 && 거기에 각각 1이 맞는지 확인 작업
+        filteredStores = filteredStores.filter(
+          (item) => item.hasOwnProperty(f) && item[f] === 1
+        );
+      });
       console.log(filteredStores);
-      // console.log(noneStores);
-      setStores(filteredStores);
-      // 초기화 작업을 따로 해줘야겠네요 
+      setStorelist(filteredStores);
     }
-    // if (stores !== undefined) {
-    //   setLoading(true);
-    // } else {
-    //   // console.log(stores);
-    // }
-    
-    // if (filter !== undefined || filter.length !== 0) {
-    //   let filteredStores = stores;
-    //   filter.map((f)=>{
-    //     console.log(f);
-    //     // key이름이 겹치자나요 키에대한 정확한 위치를 파악하고 거기에 각각 1이맞는지 확인작업까지 하는것
-    //     filteredStores = filteredStores.filter((item) => item.hasOwnProperty(f) && item[f] === 1) 
-    //   })
-    //   console.log(filteredStores);
-    //   // console.log(noneStores);
-    //   setStores(filteredStores);
-    // }
-
-    // console.log(stores);
-
-  }, [props.stores, filter]);
-  // }, [stores])
-
-  // 여기에 함수를 만들어서 stores 에 있는 json중에 카테고리가 1이 되어있는 부분을 뽑아내고 나머지는 지워서 setStores 다시 집어넣는것
-  // 두번째 방법은 내가 가지고있는 부분을 다시 백에 넘겨줘서 쿼리문으로 처리하고 다시 넘겨받기
-  // 둘다 함 수는 만들어야함
+  }, [storelist, filter]);
 
   function formatPhoneNumber(phoneNumber) {
-    const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+    const cleaned = ("" + phoneNumber).replace(/\D/g, "");
     const regex = /^(\d{3})(\d{3})(\d{4,5})$/;
-    return cleaned.replace(regex, '$1-$2-$3');
+    return cleaned.replace(regex, "$1-$2-$3");
   }
 
   return (
@@ -125,8 +89,6 @@ export default function LeftMenu({storelist}) {
           menuOpen ? "w-1/4 sm:w-3/4" : "w-14 sm:w-3"
         } opacity-100 transition-all duration-400 ease-in-out`}
       >
-        {/* 이거 부드럽게 애니메이션 주기 */}
-
         {/* 화살표(펼치기) 버튼 */}
         <BsArrowLeftShort
           className={`z-10 bg-white text-slate-400 text-6xl rounded-full absolute -right-8 top-1/3 border-4 border-slate-200 cursor-pointer 
@@ -146,18 +108,6 @@ export default function LeftMenu({storelist}) {
         >
           {/* 지역선택  */}
           <section>
-            {/* <FaDiceD6
-            className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${
-              menuOpen && "rotate-[360deg]"
-            }`}
-          /> */}
-            {/* <h1
-            className={`text-slate-600 origin-left font-medium text-2xl duration-300 ${
-              !menuOpen && "scale-0"
-            }`}
-          >
-            Manager
-          </h1> */}
             <h1 className="font-bold text-lg">지역선택</h1>
             {/* 지역-군/구 선택 */}
             <LocSelect
