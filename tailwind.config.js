3d3e007853eaf8968075992f942ac90d57bf6d57
jisu <parangwavetraveller@gmail.com>
2024-04-16 18:11:52 +0900

    #36 feat: bubble, pop animation 추가
    bubble, pop animation 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,40 @@ module.exports = {
     },
     extend: {
       keyframes: {
+        bubble: {
+          "0%": {
+            top: '27%',
+            scale: '1',
+            transform: "translateX(10px)",
+            opacity: "1",
+          },
+          "25%": {
+            transform: "translateX(-10px)",
+          },
+          "50%": {
+            transform: "translateX(10px)",
+          },
+          "75%": {
+            transform: "translateX(-10px)",
+          },
+          "100%": {
+            top: '5%',
+            scale: '0.5',
+            transform: "translateX(10px)",
+            opacity: "0"
+          },
+        },
+        pop: {
+          '0%': {
+            opacity: '0',
+          },
+          '50%': {
+            opacity: '0.7',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
         melt: {
           '0%': {
             transform: 'translateY(0)'
@@ -30,8 +64,10 @@ module.exports = {
         "sub-yellow": "#ffd60a", //
       },
       animation: {
-        melt: 'melt 1.5s ease-in-out infinite',
-      }
+        bubble: "bubble 1s ease-in-out infinite",
+        melt: "melt 1.5s ease-in-out infinite",
+        pop: "pop 1s ease-in-out 0.5s infinite",
+      },
     },
   },
   plugins: [],
