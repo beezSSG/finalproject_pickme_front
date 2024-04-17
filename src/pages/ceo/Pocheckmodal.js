9ee46fd3f9c162bf2cc10f0985b630955e94064f
hakiseong <ks510222@naver.com>
2024-04-17 18:04:16 +0900

    #50 fix 일단 머지
    #50 fix 일단 머지

--- src/pages/ceo/Pocheckmodal.js
+++ src/pages/ceo/Pocheckmodal.js
@@ -3,11 +3,9 @@ import { Modal, Button } from "antd";
 import axios from "axios";
 import Polist from "./Polist";
 
-
-export default function Pocheckmodal({getPolist, po}) {
+export default function Pocheckmodal({ getPolist, po }) {
   const [isOpen, setIsOpen] = useState(false);
   const [deleteProduct, setDeleteProduct] = useState([]);
-  
 
   // 이함수는 모달을 껏다 켰다 하는 함수
   const onToggleModal = () => {
@@ -25,90 +23,27 @@ export default function Pocheckmodal({getPolist, po}) {
     onToggleModal(); // 주소창은 자동으로 사라지므로 모달만 꺼주면 된다.
   };
 
-
   // 생각할점: 재고현황을 한번에 보는것과 먼저 상품으로 묶은뒤 상품을 클릭했을때 각기 주문한 수량 소비기한을 묶어서 보여주는것
   // #0. 승인완료 누를 때 실행되는 함수
   function con() {
-      // 받아야하는거 id, quantity, date(소비기한) 이렇게하면 완벽하게 끝! 이해가 안가는 부분 있을까요?
-      const params = {"id": po.id, "quantity": po.quantity, }; // 소비기한 , 스토어아이디
-      // #1. purchase_order 테이블에 값을 주입
-      // #2. store_product(재고) 테이블에 승인된 물건의 값을 주입
-      // 백에서 하나의 axios문으로 두개의 행동을 할것이기 때문에
-      axios.post("ceo/deleteProduct", null, {params:params})
-        .then(resp => {
-            getPolist('', '', 0);
-                    
-         })
-          .catch(err => {
-            // 오류가 발생했을 때의 처리
-            console.error(err);
-          });
-      
+    // 받아야하는거 id, quantity, date(소비기한) 이렇게하면 완벽하게 끝! 이해가 안가는 부분 있을까요?
+    const params = { id: po.id, quantity: po.quantity }; // 소비기한 , 스토어아이디
+    // #1. purchase_order 테이블에 값을 주입
+    // #2. store_product(재고) 테이블에 승인된 물건의 값을 주입
+    // 백에서 하나의 axios문으로 두개의 행동을 할것이기 때문에
+    axios
+      .post("ceo/deleteProduct", null, { params: params })
+      .then((resp) => {
+        getPolist("", "", 0);
+      })
+      .catch((err) => {
+        // 오류가 발생했을 때의 처리
+        console.error(err);
+      });
 
-      // #3. 모달창 끄기
-      onToggleModal();
+    // #3. 모달창 끄기
+    onToggleModal();
   }
-
-
-
-  //   const datepicker = $('.input__item');
-
-  //   datepicker.datepicker({
-  //       dateFormat: 'yy-mm-dd',
-  //       showOtherMonths: true,
-  //       showMonthAfterYear: true,
-  //       changeYear: true,
-  //       changeMonth: true,
-  //       showOn: 'both',
-  //       buttonImage: ('https://cdn-icons-png.flaticon.com/512/2838/2838779.png'),
-  //       buttonImageOnly: true,
-  //       showAnim: '',
-  //       onSelect: function () {
-  //           dimDisplay('none'); // hide back layer when date selected
-  //       }
-  //   });
-
-  //   $('#datepicker').datepicker('setDate', 'today'); // set date as today
-
-  
-  //   // custom datepicker 
-  //   const dateInput = document.querySelector('.input__item'),
-  //         dateIcon = document.querySelector('.ui-datepicker-trigger'),
-  //         calendar = document.querySelector('#ui-datepicker-div');
-
-  //   // add back layer
-  //   const dim = document.createElement('div');
-  //   dim.classList.add('datepicker-layer');
-
-  //   calendar.before(dim);
-
-
-  //   // back layer show
-  //   dateInput.addEventListener('focus', function () {
-  //       if (calendar.style.display === 'block') {
-  //           dimDisplay('block');
-  //       }
-  //   })
-  
-  //  dateIcon.addEventListener('click', function () {
-  //       if (calendar.style.display == 'block') {
-  //           dimDisplay('block');
-  //       }
-  //   })
-
-  //   //back layer hide
-  //   const layer = document.querySelector('.datepicker-layer');
-  
-  //   window.addEventListener('click', function (e) {
-  //       if (e.target === layer) {
-  //           dimDisplay('none');
-  //       }
-  //   });
-
-  //   // back layer display control
-  //   function dimDisplay(display) {
-  //       layer.style.display = display;
-  //   }
 
   return (
     <>
@@ -119,8 +54,8 @@ export default function Pocheckmodal({getPolist, po}) {
       >
         확인하기
       </button>
-              
-      {isOpen && 
+
+      {isOpen && (
         <Modal
           open={true}
           centered={true}
@@ -128,15 +63,14 @@ export default function Pocheckmodal({getPolist, po}) {
           onCancel={onToggleModal}
         >
           {/* 여기에 모달내부에 보여줄 디자인을 작성 */}
-          <h1 className="text-lg font-bold">
-          승인 확인되었습니다
-                  </h1>
+          <h1 className="text-lg font-bold">승인 확인되었습니다</h1>
           {/* 이부분은 유통기한이 입력되는 부분 */}
           <div className="text-center items-center">
-            <br/>
+            <br />
             <div className="text-right">
-          <input type="date" />&nbsp;&nbsp;
-            {/* <div id="wrap">
+              <input type="date" />
+              &nbsp;&nbsp;
+              {/* <div id="wrap">
               <div class="container">
                 <div class="container__wrap">
                   <h1 class="container__tit">Modal style datepicker</h1>
@@ -146,13 +80,13 @@ export default function Pocheckmodal({getPolist, po}) {
                 </div>
               </div>
             </div> */}
-          <Button key="submit" onClick={con}>
-            소비기한 입력하기
-          </Button>
-          </div>
+              <Button key="submit" onClick={con}>
+                소비기한 입력하기
+              </Button>
+            </div>
           </div>
         </Modal>
-      }
+      )}
     </>
   );
 }
--- src/pages/ceo/Polist.js
+++ src/pages/ceo/Polist.js
@@ -1,174 +1,222 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Pagination from 'react-js-pagination'; // npm i react-js-pagination
-import axios from 'axios';
-import Pocheckmodal from './Pocheckmodal';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Pagination from "react-js-pagination"; // npm i react-js-pagination
+import axios from "axios";
+import Pocheckmodal from "./Pocheckmodal";
 
 // import GlobalStyle from './styles/GlobalStyle';
 
-function Polist(){
-    const [polist, setPolist] = useState([]);
-    const [deleteProduct, setDeleteProduct] = useState([]);
-    
+function Polist() {
+  const [polist, setPolist] = useState([]);
+  const [deleteProduct, setDeleteProduct] = useState([]);
 
-    // 검색
-    const [choice, setChoice] = useState("");
-    const [search, setSearch] = useState("");
+  // 검색
+  const [choice, setChoice] = useState("");
+  const [search, setSearch] = useState("");
 
-    // 페이징
-    const [page, setPage] = useState(1);
-    const [totalCnt, setTotalCnt] = useState(0);
+  // 페이징
+  const [page, setPage] = useState(1);
+  const [totalCnt, setTotalCnt] = useState(0);
 
-    
-    useEffect(function(){
-      getPolist('', '', 0);
-    }, []);
+  useEffect(function () {
+    getPolist("", "", 0);
+  }, []);
 
-    // polist 불러오는 함수
-    function getPolist(c, s, pn){
-        axios.get("ceo/polist", 
-              {params:{ choice:c, search:s, pageNumber:pn}})
-            .then(function(resp){  // success:function
-              console.log(resp.data.polist);
-              setPolist(resp.data.polist);
-              setTotalCnt(resp.data.cnt); // 글의 총수
-            })
-            .catch(function(err){     // error:function
-              alert('error');
-            })
+  // polist 불러오는 함수
+  function getPolist(c, s, pn) {
+    axios
+      .get("ceo/polist", { params: { choice: c, search: s, pageNumber: pn } })
+      .then(function (resp) {
+        // success:function
+        console.log(resp.data.polist);
+        setPolist(resp.data.polist);
+        setTotalCnt(resp.data.cnt); // 글의 총수
+      })
+      .catch(function (err) {
+        // error:function
+        alert("error");
+      });
+  }
+
+  // 검색에 대한 부분
+  function searchBtn() {
+    // choice, search 검사
+    if (choice === "") {
+      alert("카테고리를 선택해 주십시오");
+      return;
     }
+    getPolist(choice, search, 0);
+  }
 
-    // 검색에 대한 부분
-    function searchBtn(){        
-        // choice, search 검사
-        if(choice === ''){
-            alert('카테고리를 선택해 주십시오');
-            return;
+  // 페이지 변경함수
+  function handlePageChange(page) {
+    setPage(page);
+    getPolist(choice, search, page - 1);
+  }
+  function con(po) {
+    alert(po.poYn);
+    // #1. 화면에서 승인이 완료된 물품을 사라지게 하기ㅠㅠ
+    const params = { id: po.id };
+    axios
+      .post("ceo/deleteProduct", null, { params: params })
+      .then((response) => {
+        // 응답을 받았을 때의 처리
+
+        if (po.poYn === 1) {
+          // 화면에서 승인이 완료된 물품을 사라지게 하는 작업을 수행
         }
-      getPolist(choice, search, 0);
-    }  
+      })
+      .catch((error) => {
+        // 오류가 발생했을 때의 처리
+        console.error("error");
+      });
+  }
 
-    // 페이지 변경함수
-    function handlePageChange(page){
-        setPage(page);
-        getPolist(choice, search, page-1);
-    }
-    function con(po) {
-      alert(po.poYn);
-        // #1. 화면에서 승인이 완료된 물품을 사라지게 하기ㅠㅠ
-        const params = {"id": po.id};
-          axios.post("ceo/deleteProduct", null, {params:params})
-          .then(response => {
-              // 응답을 받았을 때의 처리
-              
-              if (po.poYn === 1) {
-                  // 화면에서 승인이 완료된 물품을 사라지게 하는 작업을 수행
-              }
-            })
-            .catch(error => {
-              // 오류가 발생했을 때의 처리
-              console.error("error");
-            });
-    }
-
-    return(
-      <div>
+  return (
+    <div className="mx-auto w-[80%]">
       {/* <div className='container' style={{ marginLeft:"auto", marginRight:'auto', marginTop:"3px", marginBottom:"3px" }}>
             <p className='font-semibold text-center'>발주목록</p>
           </div>
           <br/><br/> */}
-          <table style={{ marginLeft:"auto", marginRight:'auto', marginTop:"3px", marginBottom:"3px" }} >
-            <tbody>
-              <tr>
-                <td style={{ paddingLeft:"3px" }} >
-                  <select className='mr-4 shadow-xl rounded-2xl p-5 w-[125px] focus:outline-none focus:ring-2 focus:ring-yellow-400'  value={choice} onChange={(e)=>{setChoice(e.target.value)}}>
-                  <option value="">검색</option>
-                    <option value="name">상품명</option>
-                    <option value="wdate">발주 일자</option>
-                  </select>
-                  <span class="icoArrow"><img src="https://freepikpsd.com/media/2019/10/down-arrow-icon-png-7-Transparent-Images.png" alt=""/></span>
-                </td>
-                <td style={{ paddingLeft:"5px"}} className='align-middle'>
-                  <input placeholder='카테고리를 선택해 주십시오' size='30'
-                      value={search} onChange={(e)=>{setSearch(e.target.value)}} className='rounded-2xl p-5 w-[500px] shadow-xl focus:outline-none focus:ring-2 focus:ring-yellow-400'/>
-                </td> 
-                <td style={{ paddingLeft:"5px" }}>  
-                  <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 
-                          focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-lg px-[40px] py-5 me-2 mb-2
-                          dark:focus:ring-yellow-900"  onClick={()=>{searchBtn()}}>검색</button>
-                </td>
-              </tr>                
-            </tbody>    
-          </table>
-
-          <br/><br/>
-
-          <div className='mx-auto w-[80%]'>
-          <table className='w-full table-fixed border-collapse'>
-          <thead className='bg-yellow-400 p-15' >
+      <table
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "3px",
+          marginBottom: "3px",
+        }}
+      >
+        <tbody>
           <tr>
-            <th className="w-1/6 py-2">번호</th>
+            <td style={{ paddingLeft: "3px" }}>
+              <select
+                className="mr-4 shadow-xl rounded-2xl p-5 w-[125px] focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                value={choice}
+                onChange={(e) => {
+                  setChoice(e.target.value);
+                }}
+              >
+                <option value="">검색</option>
+                <option value="name">상품명</option>
+                <option value="wdate">발주 일자</option>
+              </select>
+              <span class="icoArrow">
+                <img
+                  src="https://freepikpsd.com/media/2019/10/down-arrow-icon-png-7-Transparent-Images.png"
+                  alt=""
+                />
+              </span>
+            </td>
+            <td style={{ paddingLeft: "5px" }} className="align-middle">
+              <input
+                placeholder="카테고리를 선택해 주십시오"
+                size="30"
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+                className="rounded-2xl p-5 w-[500px] shadow-xl focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              />
+            </td>
+            <td style={{ paddingLeft: "5px" }}>
+              <button
+                className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 
+                          focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-lg px-[40px] py-5 me-2 mb-2
+                          dark:focus:ring-yellow-900"
+                onClick={() => {
+                  searchBtn();
+                }}
+              >
+                검색
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br />
+      <br />
+
+      <div className="mx-auto w-[80%]">
+        <table className="w-full table-fixed border-collapse">
+          <thead className="bg-yellow-400 p-15">
+            <tr>
+              <th className="w-1/6 py-2">번호</th>
               <th className="w-1/4 py-2">대표 이미지</th>
               <th className="w-1/2 py-2">상품명</th>
               <th className="w-1/4 py-2">수량</th>
               <th className="w-1/4 py-2">발주 일자</th>
               <th className="w-1/4 py-2">승인 여부</th>
               <th className="w-1/4 py-2">확인</th>
-          </tr>
+            </tr>
           </thead>
 
           <tbody>
-          {
-              polist.map(function(po, i){
-                return(
-                      // <TableRow po={po} rownum={i+1} key={i} />
-                      <tr className="text-center border-b hover:bg-gray-200 cursor-pointer" key={i}>
-                      <td>{ [i + 1] }</td>
-                      <td>
-
-                      {/* <img src={po.url} alt='' style={{width:140, padding:10, margin: "auto", display: "block" }}></img></td>
+            {polist.map(function (po, i) {
+              return (
+                // <TableRow po={po} rownum={i+1} key={i} />
+                <tr
+                  className="text-center border-b hover:bg-gray-200 cursor-pointer"
+                  key={i}
+                >
+                  <td>{[i + 1]}</td>
+                  <td>
+                    {/* <img src={po.url} alt='' style={{width:140, padding:10, margin: "auto", display: "block" }}></img></td>
                       <td className='text-left py-4'>{ po.name }</td> */}
-                          <img src={po.url} alt='' style={{width:140}} className='text-align'></img></td>
-                      <td className='text-center py-4'>{ po.name }</td>
+                    <img
+                      src={po.url}
+                      alt=""
+                      style={{ width: 140 }}
+                      className="text-align"
+                    ></img>
+                  </td>
+                  <td className="text-center py-4">{po.name}</td>
 
-                      <td className='text-center py-4'>{ po.quantity }</td> 
-                      <td className='text-center py-4'>{ po.wdate }</td>
-                      <td className='text-center py-4'>{ po.poYn > 0 ? <span style={{ color: "red", fontWeight:"bold" }}>승인완료</span>  : '승인대기중'}</td>  
-                      <td>
-                        {/* 여기에 모달을 추가하면 거기안에 버튼까지 있으니까 괜찮을 거예요 */}
-                        {/* 만약 모달에서 값을 전달해야하거나 전달받아야하면 아래의 컴포넌트에서 상호작용 하시면 됩니다 */}
-                        { po.poYn === 1 ? <Pocheckmodal po={po} getPolist={getPolist()} /> : ""}
-                        {/* <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900" 
+                  <td className="text-center py-4">{po.quantity}</td>
+                  <td className="text-center py-4">{po.wdate}</td>
+                  <td className="text-center py-4">
+                    {po.poYn > 0 ? (
+                      <span style={{ color: "red", fontWeight: "bold" }}>
+                        승인완료
+                      </span>
+                    ) : (
+                      "승인대기중"
+                    )}
+                  </td>
+                  <td>
+                    {/* 여기에 모달을 추가하면 거기안에 버튼까지 있으니까 괜찮을 거예요 */}
+                    {/* 만약 모달에서 값을 전달해야하거나 전달받아야하면 아래의 컴포넌트에서 상호작용 하시면 됩니다 */}
+                    {po.poYn === 1 ? (
+                      <Pocheckmodal po={po} getPolist={getPolist()} />
+                    ) : (
+                      ""
+                    )}
+                    {/* <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900" 
                         onClick={()=>{con(po)}}>승인확인</button> */}
-                      </td> 
-                  </tr>
-                );     
-              })
-          }
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
-          </table>
-          </div>
-          <br/>
-
-          {/* 세번째의 경우 */}     
-          <Pagination
-            itemClass='page-item'
-            linkClass='page-link' 
-            activePage={page}
-            itemsCountPerPage={10}
-            totalItemsCount={totalCnt}
-            pageRangeDisplayed={10}
-            prevPageText={"prev"}
-            nextPageText={"next"}
-            onChange={handlePageChange} />
-
-        <div className='my-5 d-flex justify-content-center'>
-            <Link  style={{ marginLeft:"auto", marginRight:'auto', marginTop:"3px", marginBottom:"3px" }} className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"to="/pow">발주신청</Link>
-        </div>       
-
+        </table>
       </div>
-    );
-}
+      <br />
 
+      {/* 세번째의 경우 */}
+      <Pagination
+        itemClass="page-item"
+        linkClass="page-link"
+        activePage={page}
+        itemsCountPerPage={10}
+        totalItemsCount={totalCnt}
+        pageRangeDisplayed={10}
+        prevPageText={"prev"}
+        nextPageText={"next"}
+        onChange={handlePageChange}
+      />
+      <br />
+    </div>
+  );
+}
 
 export default Polist;
