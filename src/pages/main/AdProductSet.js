f4934535285e55abba18b7462394559bf2cb8f57
jisu <parangwavetraveller@gmail.com>
2024-04-18 17:02:05 +0900

    #49 style: 이달의 행사상품 swiper 추가
    이달의 행사상품 swiper 추가 및 swiper 버튼 수정

--- src/pages/main/AdProductSet.js
+++ src/pages/main/AdProductSet.js
@@ -1,5 +1,3 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -8,10 +6,53 @@ import "swiper/css/pagination";
 import "../../styles/main/PromotionStyle.css";
 
 // import required modules
-import { Autoplay, Keyboard, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Keyboard, Autoplay } from "swiper/modules";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const tags = [
+  {
+    id: 1,
+    name: "1 + 1",
+  },
+  {
+    id: 2,
+    name: "2 + 1",
+  },
+  {
+    id: 3,
+    name: "신상품",
+  },
+];
 
 const AdProductSet = () => {
+  const [selectedTag, setSelectedTag] = useState(1);
+  const [products, setProducts] = useState([]);
+
+  function getProducts(tag) {
+    axios
+      .get("/product/promotedproductlist", { params: { tag: tag } })
+      .then((resp) => {
+        // console.log(resp.data);
+        setProducts(resp.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+
+  const handleSelectedTag = (tagId) => {
+    console.log(tagId);
+    setSelectedTag(tagId);
+  };
+
+  useEffect(() => {
+    // console.log(selectedTag);
+    getProducts(selectedTag); // 페이지가 처음으로 로드될 때 기본 태그에 맞는 상품 가져오기
+  }, [selectedTag]);
+
   return (
     <>
       <div className="bg-white rounded-2xl m-auto mb-11 drop-shadow-2xl">
@@ -20,23 +61,51 @@ const AdProductSet = () => {
             <h1 className="lg:text-4xl md:text-3xl sm:text-2xl font-bold tracking-tight text-slate-900">
               이달의 행사 상품
             </h1>
-            <Link className="text-slate-500 lg:text-xl md:text-lg sm:text-lg font-bold tracking-tight hover:text-slate-800 transition duration-300"
-                  to="">
+            <Link
+              className="text-slate-500 lg:text-xl md:text-lg sm:text-lg font-bold tracking-tight hover:text-slate-800 transition duration-300"
+              to=""
+            >
               더보기
             </Link>
           </div>
 
           {/* 상품 분류 태그 */}
+          <ul className="flex flex-wrap my-4 gap-2">
+            {tags.map((tag) => (
+              <li className="mb-1.5 mx-1" key={tag.id}>
+                <input
+                  value={tag.id}
+                  type="radio"
+                  className="hidden peer"
+                  name="tag"
+                  id={tag.name}
+                  onChange={()=>handleSelectedTag(tag.id)}
+                  checked={selectedTag === tag.id}
+                />
+                <label
+                  htmlFor={tag.name}
+                  className="inline-flex items-center justify-between p-3 text-xl font-extrabold text-center text-slate-600 border-2
+                          border-slate-300 rounded-full cursor-pointer transition duration-300 ease-in-out 
+                          hover:bg-main-orange hover:border-sub-orange hover:text-slate-900 peer-checked:text-slate-900
+                          peer-checked:bg-main-orange peer-checked:border-sub-orange"
+                >
+                  <div className="w-full text-xl font-extrabold text-center">
+                    {tag.name}
+                  </div>
+                </label>
+              </li>
+            ))}
+          </ul>
           {/* 상품 목록 */}
 
           {/* 행사 상품 슬라이더 */}
           <Swiper
             slidesPerView={3}
-            spaceBetween={20}
+            spaceBetween={30}
             loop={true}
             centeredSlides={true}
             autoplay={{
-              delay: 2000,
+              delay: 3500,
               disableOnInteraction: false,
             }}
             keyboard={{
@@ -45,10 +114,19 @@ const AdProductSet = () => {
             pagination={{
               clickable: true,
             }}
-            // navigation={true}
-            modules={[Autoplay, Keyboard, Pagination]}
-            className="mySwiper AdProductSet__Slider"
+            navigation={true}
+            modules={[Autoplay, Keyboard, Pagination, Navigation]}
+            className="mySwiper"
           >
+            { 
+              products &&
+              products.map((product) => (
+                <SwiperSlide key={product.id} className="flex flex-col py-[5%]">
+                  <img src={product.url} alt={`이벤트상품 ${product.id}`} />
+                  <h1 className="py-[3%] font-bold text-center text-slate-800 lg:text-xl md:text-lg sm:text-sm">{product.name}</h1>
+                </SwiperSlide>
+              ))
+            }
           </Swiper>
         </div>
       </div>
