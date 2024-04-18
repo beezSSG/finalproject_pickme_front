42aab5bd09015462e5f3328c2a2e16899c239e38
jisu <parangwavetraveller@gmail.com>
2024-04-18 14:30:20 +0900

    #49 style: 메인홈 - 상품 검색 및 카테고리 리팩토링
    사용하지 않는 comp 삭제

--- src/pages/main/SearchProduct.js
+++ src/pages/main/SearchProduct.js
@@ -67,28 +67,6 @@ const SearchProduct = () => {
               />
 
               <div>
-                {/* 음성검색 버튼 */}
-                {/* <button
-                  type="button"
-                  className="absolute inset-y-0 end-0 flex items-center pe-12
-                            text-gray-400 hover:text-gray-700 transition-colors duration-400 ease-in-out "
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
-                    />
-                  </svg>
-                </button> */}
-
                 {/* 검색 버튼 */}
                 <button
                   type="button"
@@ -114,10 +92,6 @@ const SearchProduct = () => {
                 </button>
               </div>
             </div>
-            {/* <button
-              type="submit"
-              // className="inline-flex items-center py-5 px-3 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            > */}
           </form>
 
           {/* 상품 카테고리 아이콘 */}
